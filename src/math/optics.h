7a044cab41fb89be8508e79c55041f88881e7646
zhuling1 <249002031@qq.com>
2022-09-06 23:08:23 +0800

    update math lib

---
+++ src/math/optics.h
@@ -0,0 +1,29 @@
+//
+// Created by Zero on 06/09/2022.
+//
+
+#pragma once
+
+#include "ocarina/src/core/basic_types.h"
+#include "ocarina/src/dsl/common.h"
+
+using namespace ocarina;
+
+namespace vision {
+template<typename T>
+requires ocarina::is_vector3_v<expr_value_t<T>>
+[[nodiscard]] T reflect(const T &wo, const T &n) {
+    return -wo + 2 * dot(wo, n) * n;
+}
+
+template<typename T>
+[[nodiscard]] T schlick_weight(const T &cos_theta) {
+    T m = clamp(1.f - cos_theta, 0.f, 1.f);
+    return Pow<5>(m);
+}
+
+template<typename T, typename U>
+[[nodiscard]] auto fresnel_schlick(const T &R0, const U &cos_theta) {
+    return lerp(schlick_weight(cos_theta), R0, T{1.f});
+}
+}// namespace vision
