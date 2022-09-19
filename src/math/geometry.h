250d2963595a2fca20dbcb408fc4326dba74c95e
zhuling1 <249002031@qq.com>
2022-09-20 00:45:31 +0800

    update frame

--- src/math/geometry.h
+++ src/math/geometry.h
@@ -17,13 +17,68 @@ struct Triangle {
 OC_STRUCT(vision::Triangle, i, j, k){};
 
 namespace vision {
+using namespace ocarina;
 inline namespace geometry {
-struct Frame {
-    float3 x, y, z;
-};
+
+template<typename T>
+requires is_vector3_expr_v<T>
+[[nodiscard]] auto cos_theta_2(const T &v) noexcept { return sqr(v.z); }
+
+template<typename T>
+requires is_vector3_expr_v<T>
+[[nodiscard]] auto cos_theta(const T &v) noexcept { return v.z; }
+
+template<typename T>
+requires is_vector3_expr_v<T>
+[[nodiscard]] auto abs_cos_theta(const T &v) noexcept{ return ocarina::abs(v.z); }
+
+template<typename T>
+requires is_vector3_expr_v<T>
+[[nodiscard]] auto sin_theta_2(const T &v)noexcept { return 1.0f - cos_theta_2(v); }
+
+template<typename T>
+requires is_vector3_expr_v<T>
+[[nodiscard]] auto sin_theta(const T &v)noexcept {
+    auto temp = sin_theta_2(v);
+    return select(temp <= 0.f, 0.f, sqrt(temp));
 }
-}// namespace vision::geometry
-OC_STRUCT(vision::Frame, x, y, z){};
+
+template<typename T>
+requires is_vector3_expr_v<T>
+[[nodiscard]] auto tan_theta(const T &v) noexcept {
+    auto sin_theta2 = 1 - cos_theta_2(v);
+    if (sin_theta2 <= 0.0f) {
+        return 0.0f;
+    }
+    return std::sqrt(sin_theta2) / cos_theta(v);
+}
+
+template<typename T>
+requires is_vector3_expr_v<T>
+struct Frame {
+public:
+    using vec_ty = T;
+    vec_ty x, y, z;
+
+public:
+    Frame(const T &x, const T &y, const T &z)
+        : x(x), y(y), z(z) {}
+
+    explicit Frame(const T &normal)
+        : z(normal) {
+        ocarina::coordinate_system(z, x, y);
+    }
+
+    [[nodiscard]] vec_ty to_local(vec_ty world_v) const noexcept {
+        return vec_ty(dot(world_v, x), dot(world_v, y), dot(world_v, z));
+    }
+
+    [[nodiscard]] vec_ty to_world(vec_ty local_v) const noexcept {
+        return x * local_v.x + y * local_v.y + z * local_v.z;
+    }
+};
+}// namespace geometry
+}// namespace vision
 
 namespace vision {
 inline namespace geometry {
