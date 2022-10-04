82a65c3d27e878620dd02f688e55080fe624f3ce
zhuling1 <249002031@qq.com>
2022-10-04 13:15:29 +0800

    update interaction

---
+++ src/base/interaction.h
@@ -0,0 +1,46 @@
+//
+// Created by Zero on 03/10/2022.
+//
+
+#pragma once
+
+#include "core/basic_types.h"
+#include "dsl/common.h"
+#include "math/geometry.h"
+
+namespace vision {
+
+using namespace ocarina;
+
+template<typename T>
+requires is_vector3_expr_v<T>
+struct UVN : Frame<T> {
+public:
+    using vec_ty = T;
+
+public:
+    void set_frame(Frame<T> frame) {
+        this->x = frame.x;
+        this->y = frame.y;
+        this->z = frame.z;
+    }
+    [[nodiscard]] vec_ty dp_du() const noexcept { return this->x; }
+    [[nodiscard]] vec_ty dp_dv() const noexcept { return this->y; }
+    [[nodiscard]] vec_ty normal() const noexcept { return this->z; }
+};
+
+template<typename T>
+requires is_vector3_expr_v<T>
+struct Interaction {
+public:
+    using vec_ty = T;
+    using scalar_ty = scalar_t<vec_ty>;
+
+public:
+    vec_ty pos;
+    vec_ty wo;
+    scalar_ty time;
+    UVN<vec_ty> g_uvn;
+};
+
+}// namespace vision
--- src/base/light.h
+++ src/base/light.h
@@ -3,3 +3,25 @@
 //
 
 #pragma once
+
+#include "interaction.h"
+#include "scene_node.h"
+
+namespace vision {
+
+// LightType Definition
+enum class LightType : uint8_t {
+    DeltaPosition,
+    DeltaDirection,
+    Area,
+    Infinite
+};
+
+class Light : public SceneNode {
+protected:
+    const LightType _type{LightType::Area};
+
+public:
+    explicit Light(LightType light_type) : _type(light_type) {}
+};
+}// namespace vision
