12acfb1af81a2e59a15b7163cc426acdf79a43ec
zhuling1 <249002031@qq.com>
2022-10-15 23:57:20 +0800

    update sampler

--- src/base/sampler.h
+++ src/base/sampler.h
@@ -8,6 +8,15 @@
 #include "node.h"
 
 namespace vision {
+using namespace ocarina;
 class Sampler : public Node {
+public:
+    using Desc = SamplerDesc;
+
+protected:
+    uint _spp{1u};
+
+public:
+    explicit Sampler(const SamplerDesc *desc) : Node(desc), _spp(desc->spp) {}
 };
 }// namespace vision
--- src/samplers/independent.cpp
+++ src/samplers/independent.cpp
@@ -8,6 +8,10 @@ namespace vision {
 using namespace ocarina;
 
 class IndependentSampler : public Sampler {
+public:
+    explicit IndependentSampler(const SamplerDesc *desc) : Sampler(desc) {}
 };
 
 }// namespace vision
+
+VS_MAKE_CLASS_CREATOR(vision::IndependentSampler)
