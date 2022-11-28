dd4a9948d97e5099a5c1920d7c4136137d621707
zhuling1 <249002031@qq.com>
2022-11-28 22:28:02 +0800

    update projector

--- src/lights/projector.cpp
+++ src/lights/projector.cpp
@@ -19,7 +19,7 @@ public:
     explicit Projector(const LightDesc &desc)
         : IPointLight(desc),
           _ratio(desc.ratio),
-          _angle_y(desc.angle),
+          _angle_y(radians(desc.angle)),
           _o2w(desc.o2w.mat),
           _scale(desc.scale) {
         _intensity = desc.scene->load<Texture>(desc.texture_desc);
@@ -31,8 +31,15 @@ public:
     [[nodiscard]] float3 position() const noexcept override { return _o2w[3].xyz(); }
     [[nodiscard]] Float3 Li(const LightSampleContext &p_ref,
                             const LightEvalContext &p_light) const noexcept override {
-        // todo
+        Float3 p = transform_point(inverse(_o2w), p_ref.pos);
+        Float3 w = p / p.z;
+        float tan_y = tan(_angle_y);
+        float tan_x = _ratio * tan_y;
+        Float u = (p.x + tan_x) / (2 * tan_x);
+        Float v = (p.y + tan_y) / (2 * tan_y);
         return make_float3(0.f);
+//        print("{},{},{}", p.x,p.y,p.z);
+//        return _intensity->eval(make_float2(u,v)).xyz() / length(p) * _scale;
     }
 };
 }// namespace vision
