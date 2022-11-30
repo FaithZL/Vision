849d831b4be1d2aace78229ccd8c8c2417ab1c8d
zhuling1 <249002031@qq.com>
2022-11-30 17:39:27 +0800

    Update environment.cpp

--- src/lights/environment.cpp
+++ src/lights/environment.cpp
@@ -12,30 +12,58 @@ namespace vision {
 class EnvironmentLight : public Light {
 private:
     Warper2D *_warper;
-    float4x4 _o2w;
+    float4x4 _w2o;
     float _scale{1.f};
     Texture *_texture{nullptr};
 
 public:
     explicit EnvironmentLight(const LightDesc &desc)
         : Light(desc, LightType::Infinite),
-          _o2w(desc.o2w.mat),
+          _w2o(inverse(desc.o2w.mat)),
           _scale(desc.scale),
           _texture(desc.scene->load<Texture>(desc.texture_desc)) {}
 
+    [[nodiscard]] Float2 UV(Float3 local_dir) const {
+        return make_float2(spherical_phi(local_dir) * Inv2Pi, spherical_phi(local_dir) * InvPi);
+    }
+
+    [[nodiscard]] Float3 L(Float3 local_dir) const {
+        Float2 uv = UV(local_dir);
+        return _texture->eval(uv).xyz();
+    }
+
     [[nodiscard]] Float3 Li(const LightSampleContext &p_ref, const LightEvalContext &p_light) const noexcept override {
-        // todo
-        return make_float3(0.f);
+        Float3 direction = normalize(p_light.pos - p_ref.pos);
+        return L(transform_vector(_w2o, direction));
     }
 
     [[nodiscard]] Float PDF_Li(const LightSampleContext &p_ref, const LightEvalContext &p_light) const noexcept override {
-        // todo
-        return 0.f;
+        Float3 direction = normalize(p_light.pos - p_ref.pos);
+        direction = transform_vector(_w2o, direction);
+        Float theta = spherical_theta(direction);
+        Float phi = spherical_phi(direction);
+        Float sin_theta = sin(theta);
+        Float2 uv = make_float2(phi * Inv2Pi, theta * InvPi);
+        Float pdf = _warper->PDF(uv) / (_2Pi * Pi * sin_theta);
+        return select(sin_theta == 0, 0.f, pdf);
     }
 
     [[nodiscard]] LightSample sample_Li(const LightSampleContext &p_ref, Float2 u) const noexcept override {
-        // todo
-        return LightSample{};
+        LightSample ret;
+        auto [uv, pdf_map, coord] = _warper->sample_continuous(u);
+        Float theta = uv[1] * Pi;
+        Float phi = uv[0] * _2Pi;
+        Float sin_theta = sin(theta);
+        Float cos_theta = cos(theta);
+        Float3 direction = spherical_direction(sin_theta, cos_theta, phi);
+        direction = normalize(transform_vector(_w2o, direction));
+        Float pdf_dir = pdf_map / (_2Pi * Pi * sin_theta);
+        Float3 pos = p_ref.pos + direction;
+        LightEvalContext p_light;
+        p_light.pos = pos;
+        ret.eval = evaluate(p_ref, p_light);
+        ret.p_light = pos;
+        return ret;
     }
 
     [[nodiscard]] vector<float> calculate_weights() noexcept {
