b490fa215d0494bd277f9f4c31f5bd91aa79b9e9
zhuling1 <249002031@qq.com>
2022-12-21 23:32:46 +0800

    Update test_bxdf.cpp

--- src/tests/test_bxdf.cpp
+++ src/tests/test_bxdf.cpp
@@ -20,14 +20,14 @@ int main(int argc, char *argv[]) {
     float ax = 0.01;
     float ay = 0.01;
 
-    Microfacet<H> mf(ax, ay);
-    float3 wo = normalize(make_float3(1,1,0.6));
-    auto wh = mf.sample_wh(wo, u);
+//    Microfacet<H> mf(ax, ay);
+//    float3 wo = normalize(make_float3(1,1,0.6));
+//    auto wh = mf.sample_wh(wo, u);
 //    wh = make_float3(0,0,1);
 //    auto pdf = mf.PDF_wh(wo, wh);
 //    cout << pdf << endl;
 //    auto wi = reflect(wo, wh);
-    cout << format("{} {} {}", wh.x, wh.y, wh.z);
+//    cout << format("{} {} {}", wh.x, wh.y, wh.z);
 //    print("{} {} {}", wh.x, wh.y, wh.z);
 //    auto brdf = mf.BRDF(wo, wi, make_float3(1.f), wo.z, wi.z);
     return 0;
