76b63d3cf0175b7637fc977145a2fd26b0a4c3c6
Zero <249002031@qq.com>
2022-12-30 23:41:59 +0800

    Update test_playground.cpp

--- src/tests/test_playground.cpp
+++ src/tests/test_playground.cpp
@@ -15,37 +15,41 @@ using namespace vision;
 using namespace ocarina;
 int main(int argc, char *argv[]) {
 
-    float a = bit_cast<float>(bit_cast<uint>(empty_range_upper<float>()) - 1);
-    float b = bit_cast<float>(bit_cast<uint>(empty_range_upper<float>()) - 10000);
-    b = 100;
 
-    cout << a << endl;
-    cout << b << endl;
-    cout << a - b << endl;
-    cout << a / (a + b) << endl;
-    cout << a / b << endl;
+    fs::path path(argv[0]);
+    Context context(path.parent_path());
+    Device device = context.create_device("cuda");
+    Stream stream = device.create_stream();
+    context.clear_cache();
+    float3 vec = make_float3(2);
+    float4x4 mat = scale(2);
+    Transform tsf = Transform(mat);
 
-//    fs::path path(argv[0]);
-//    Context context(path.parent_path());
-//    Device device = context.create_device("cuda");
-//    Stream stream = device.create_stream();
-//    context.clear_cache();
-//    float3 vec = make_float3(2);
-//    float4x4 mat = scale(2);
-//    Transform tsf = Transform(mat);
-//
-//    auto ttt = Transform(scale(0.1f));
-//
-//    auto m = rotation(make_float3(1.f), 30.f, false);
-//
-//    vec = tsf.apply_point(vec);
-//
-//    Kernel kernel = [&]() {
+    auto ttt = Transform(scale(0.1f));
+
+    auto m = rotation<H>(make_float3(1.f), 30.f, false);
+
+    vec = tsf.apply_point(vec);
+
+    int size = sizeof(sRGBToSpectrumTable_Data) / sizeof(float);
+
+    Buffer<float> buf = device.create_buffer<float>(size);
+
+    buf.upload_immediately(sRGBToSpectrumTable_Data);
+
+    Kernel kernel = [&]() {
 //        Var f3 = make_float3(1,2,1);
 //        Frame frame = Frame(f3);
-//
+//        prints("{} {} {}",f3.xyz());
 //        f3 = frame.to_local(f3);
-//    };
-//    auto shader = device.compile(kernel);
+//        prints("{} {} {}",f3.xyz());
+        $for(i,0,10) {
+            prints("{}", buf.read(i));
+        };
+    };
+    auto shader = device.compile(kernel);
+
+    stream << shader().dispatch(1) << synchronize() << commit();
+
     return 0;
 }
