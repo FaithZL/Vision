e85a0c18c6a67ec02a8ac6d602e40e4bf25b9f5f
Zero <249002031@qq.com>
2023-02-01 15:17:10 +0800

    Update test_playground.cpp

--- src/tests/test_playground.cpp
+++ src/tests/test_playground.cpp
@@ -15,7 +15,14 @@
 using namespace vision;
 using namespace ocarina;
 
-float eta(float lambda) {
+float eta_bk7(float lambda) {
+    float f = 1.03961212 * sqr(lambda) / (sqr(lambda) - 0.00600069867) +
+        0.231792344 * sqr(lambda) / (sqr(lambda) - 0.0200179144) +
+        1.01046945 * sqr(lambda) / (sqr(lambda) - 103.560653);
+    return sqrt(f + 1);
+}
+
+float eta_LASF9(float lambda) {
     auto f = 2.00029547f * sqr(lambda) / (sqr(lambda) - 0.0121426017f) +
         0.298926886f * sqr(lambda) / (sqr(lambda) - 0.0538736236f) +
         1.80691843f * sqr(lambda) / (sqr(lambda) - 156.530829f);
@@ -24,7 +31,7 @@ float eta(float lambda) {
 
 int main(int argc, char *argv[]) {
 
-    float e = eta(0.83);
-    float e2 = eta(0.36);
+    float e = eta_LASF9(0.83);
+    float e2 = eta_LASF9(0.36);
     return 0;
 }
