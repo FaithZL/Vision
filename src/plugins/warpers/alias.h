0767c403ce8c400cb3c10f0916fcb7e03b81a02e
Zero <249002031@qq.com>
2023-02-07 10:21:13 +0800

    整理代码

--- src/plugins/warpers/alias.h
+++ src/plugins/warpers/alias.h
@@ -28,8 +28,8 @@ private:
     friend class AliasTable2D;
 
 public:
-    explicit AliasTable(ResourceArray &bindless_array)
-        : _table(bindless_array), _func(bindless_array) {}
+    explicit AliasTable(ResourceArray &resource_array)
+        : _table(resource_array), _func(resource_array) {}
     explicit AliasTable(const WarperDesc &desc)
         : Warper(desc),
           _table(render_pipeline()->resource_array()),
