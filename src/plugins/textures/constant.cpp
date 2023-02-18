f6a6863fdc2b01ff4d79bda8c223e595d76af6bf
Zero <249002031@qq.com>
2023-02-18 21:15:06 +0800

    Update constant.cpp

--- src/plugins/textures/constant.cpp
+++ src/plugins/textures/constant.cpp
@@ -3,6 +3,7 @@
 //
 
 #include "base/texture.h"
+#include "base/mgr/render_pipeline.h"
 
 namespace vision {
 class ConstantTexture : public Texture {
@@ -11,6 +12,9 @@ private:
 
 public:
     explicit ConstantTexture(const TextureDesc &desc) : Texture(desc), _val(desc.val) {}
+    void prepare() noexcept override {
+        
+    }
     [[nodiscard]] bool is_zero() const noexcept override { return ocarina::is_zero(_val); }
     [[nodiscard]] Float4 eval(const TextureEvalContext &tev) const noexcept override { return _val; }
     [[nodiscard]] Float4 eval(const Float2 &uv) const noexcept override { return _val; }
