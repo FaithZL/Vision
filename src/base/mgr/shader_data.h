1cb8442f822ed7e39ed458d718f10ed327f1f2d0
Zero <249002031@qq.com>
2023-03-01 11:43:08 +0800

    Update shader_data.h

--- src/base/mgr/shader_data.h
+++ src/base/mgr/shader_data.h
@@ -10,7 +10,12 @@ namespace vision {
 using namespace ocarina;
 class ShaderData : public ocarina::Managed<char> {
 public:
+    template<typename T>
+    [[nodiscard]] uint add(T t) noexcept {
+        uint ret = _size;
 
+        return ret;
+    }
 };
 
 }// namespace vision
