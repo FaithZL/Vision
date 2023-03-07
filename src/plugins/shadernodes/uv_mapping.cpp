a6a9c054e68199bb342b90801f7be45c3f6cf4be
Zero <249002031@qq.com>
2023-03-07 21:20:22 +0800

    update node

---
+++ src/plugins/shadernodes/uv_mapping.cpp
@@ -0,0 +1,17 @@
+//
+// Created by Zero on 07/03/2023.
+//
+
+#include "base/shader_node.h"
+
+namespace vision {
+class UVMapping : public ShaderNode {
+private:
+    float2 _offset{};
+    float2 _scale{};
+
+public:
+    explicit UVMapping(const ShaderNodeDesc &desc)
+        : ShaderNode(desc) {}
+};
+}// namespace vision
