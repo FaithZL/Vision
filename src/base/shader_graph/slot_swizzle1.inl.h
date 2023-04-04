aa5b599a8c2a5e3aeddd9e748bb80c0c3e3c295a
Zero <249002031@qq.com>
2023-04-04 18:40:09 +0800

    del

--- src/base/shader_graph/slot_swizzle1.inl.h
+++
@@ -1,4 +0,0 @@
-case 0x0: return _node->evaluate(ctx, da).x();
-case 0x1: return _node->evaluate(ctx, da).y();
-case 0x2: return _node->evaluate(ctx, da).z();
-case 0x3: return _node->evaluate(ctx, da).w();
