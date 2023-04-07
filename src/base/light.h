92ba6b9bc8be6cf1f6cc94835c0dcc74b2a59b1d
Zero <249002031@qq.com>
2023-04-07 10:20:28 +0800

    Update light.h

--- src/base/light.h
+++ src/base/light.h
@@ -31,6 +31,9 @@ protected:
 
 public:
     explicit Light(const LightDesc &desc, LightType light_type);
+    [[nodiscard]] uint64_t _compute_type_hash() const noexcept override {
+        return _color.type_hash();
+    }
     [[nodiscard]] virtual SampledSpectrum Li(const LightSampleContext &p_ref, const LightEvalContext &p_light, const SampledWavelengths &swl) const noexcept = 0;
     [[nodiscard]] virtual Float PMF(const Uint &prim_id) const noexcept { return 0.f; }
     [[nodiscard]] virtual Float PDF_Li(const LightSampleContext &p_ref, const LightEvalContext &p_light) const noexcept = 0;
