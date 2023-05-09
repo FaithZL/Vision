34e5160a74b5815e09a17bf41ac672887fb06184
Zero <249002031@qq.com>
2023-05-09 23:50:50 +0800

    update light sampler

--- src/render_core/lightsamplers/bvh.cpp
+++ src/render_core/lightsamplers/bvh.cpp
@@ -1,3 +1,15 @@
 //
 // Created by Zero on 28/10/2022.
 //
+
+#include "base/lightsampler.h"
+#include "base/mgr/render_pipeline.h"
+#include "base/sampler.h"
+
+namespace vision {
+class LightBVHSampler : public LightSampler {
+public:
+    explicit LightBVHSampler(const LightSamplerDesc &desc)
+        : LightSampler(desc) {}
+};
+}// namespace vision
