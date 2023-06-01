65ffff4a8166746ad94718ae88dd4a28640573f7
Zero <249002031@qq.com>
2023-06-01 16:33:38 +0800

    update postprocessor

---
+++ src/base/mgr/postprocessor.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Zero on 2023/6/1.
+//
+
+#include "postprocessor.h"
---
+++ src/base/mgr/postprocessor.h
@@ -0,0 +1,14 @@
+//
+// Created by Zero on 2023/6/1.
+//
+
+#pragma once
+
+#include "base/denoiser.h"
+
+namespace vision {
+class Postprocessor {
+private:
+
+};
+}
