057d15b5531004ea83dd25ec364534a2254cab38
Zero <249002031@qq.com>
2023-06-07 00:41:00 +0800

    Update multiply.cpp

--- src/render_core/shadernodes/multiply.cpp
+++ src/render_core/shadernodes/multiply.cpp
@@ -3,6 +3,7 @@
 //
 
 #include "base/shader_graph/shader_node.h"
+#include "base/mgr/scene.h"
 
 namespace vision {
 class Multiply : public ShaderNode {
@@ -11,6 +12,16 @@ private:
     Slot _rhs;
 
 public:
-    explicit Multiply(const ShaderNodeDesc &desc) : ShaderNode(desc) {}
+    explicit Multiply(const ShaderNodeDesc &desc)
+        : ShaderNode(desc),
+          _lhs(desc.scene->create_slot(*desc.slot("lhs", desc["lhs"].data(), Albedo))),
+          _rhs(desc.scene->create_slot(*desc.slot("rhs", desc["rhs"].data(), Albedo))) {}
+
+    [[nodiscard]] bool is_uniform() const noexcept override {
+        return _lhs->is_uniform() && _rhs->is_uniform();
+    }
+    [[nodiscard]] bool is_constant() const noexcept override {
+        return _lhs->is_constant() && _rhs->is_constant();
+    }
 };
 }// namespace vision
