d866565412c36ad1e8586dbf3487ebb2d21f4e9e
Zero <249002031@qq.com>
2023-06-19 14:14:50 +0800

    update ao

--- src/render_core/integrators/ao.cpp
+++ src/render_core/integrators/ao.cpp
@@ -24,6 +24,44 @@ public:
 
     OC_SERIALIZABLE_FUNC(Integrator, _distance, _cos_sample, _sample_num)
 
+    [[nodiscard]] Float3 Li(vision::RayState &rs) const noexcept override {
+        Float3 L = make_float3(0.f);
+        Pipeline *rp = pipeline();
+        Camera *camera = scene().camera();
+        Sampler *sampler = scene().sampler();
+        Geometry &geom = rp->geometry();
+        $while(true) {
+            Var hit = geom.trace_closest(rs.ray);
+            $if(hit->is_miss()) {
+                $break;
+            };
+            Interaction it = geom.compute_surface_interaction(hit, rs.ray);
+            $if(!it.has_material()) {
+                rs = it.spawn_ray_state(rs.direction());
+                $continue;
+            };
+            Float3 wi;
+            Float pdf;
+            $for(i, *_sample_num) {
+                if (_cos_sample.hv()) {
+                    wi = square_to_cosine_hemisphere(sampler->next_2d());
+                    pdf = cosine_hemisphere_PDF(geometry::abs_cos_theta(wi));
+                } else {
+                    wi = square_to_hemisphere(sampler->next_2d());
+                    pdf = uniform_hemisphere_PDF();
+                }
+                it.s_uvn.z = face_forward(it.s_uvn.normal(), -rs.direction());
+                wi = it.s_uvn.to_world(wi);
+                Bool occ = geom.trace_any(it.spawn_ray(wi, *_distance));
+                $if(!occ) {
+                    L += dot(wi, it.s_uvn.normal()) / (pdf * *_sample_num);
+                };
+            };
+            $break;
+        };
+        return L;
+    }
+
     void compile_shader() noexcept override {
         Pipeline *rp = pipeline();
         Camera *camera = scene().camera();
@@ -35,40 +73,10 @@ public:
             _frame_index = frame_index;
             sampler->start_pixel_sample(pixel, frame_index, 0);
             SensorSample ss = sampler->sensor_sample(pixel, camera->filter());
+            camera->load_data();
             RayState rs = camera->generate_ray(ss);
-            Float3 L = make_float3(0.f);
 
-            $while(true) {
-                Var hit = geom.trace_closest(rs.ray);
-                $if(hit->is_miss()) {
-                    $break;
-                };
-                Interaction it = geom.compute_surface_interaction(hit, rs.ray);
-                $if(!it.has_material()) {
-                    rs = it.spawn_ray_state(rs.direction());
-                    $continue;
-                };
-                Float3 wi;
-                Float pdf;
-                $for(i, *_sample_num) {
-                    if (_cos_sample.hv()) {
-                        wi = square_to_cosine_hemisphere(sampler->next_2d());
-                        pdf = cosine_hemisphere_PDF(geometry::abs_cos_theta(wi));
-                    } else {
-                        wi = square_to_hemisphere(sampler->next_2d());
-                        pdf = uniform_hemisphere_PDF();
-                    }
-                    it.s_uvn.z = face_forward(it.s_uvn.normal(), -rs.direction());
-                    wi = it.s_uvn.to_world(wi);
-                    Bool occ = geom.trace_any(it.spawn_ray(wi, *_distance));
-                    $if(!occ) {
-                        L += dot(wi, it.s_uvn.normal()) / (pdf * *_sample_num);
-                    };
-                };
-                $break;
-            };
-
-            camera->radiance_film()->add_sample(pixel, L, frame_index);
+            camera->radiance_film()->add_sample(pixel, Li(rs), frame_index);
         };
         _shader = rp->device().compile(kernel);
     }
