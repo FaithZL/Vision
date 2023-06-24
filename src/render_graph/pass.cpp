638531e6355430c2503a6673bd8875cf88e04b76
Zero <249002031@qq.com>
2023-06-24 17:15:48 +0800

    update render graph

---
+++ src/render_graph/pass.cpp
@@ -0,0 +1,6 @@
+//
+// Created by Zero on 2023/6/24.
+//
+
+#include "pass.h"
+
---
+++ src/render_graph/pass.h
@@ -0,0 +1,20 @@
+//
+// Created by Zero on 2023/6/24.
+//
+
+#pragma once
+
+#include "rhi/common.h"
+
+namespace vision {
+
+using namespace ocarina;
+
+class Pass {
+public:
+    virtual void setup() noexcept = 0;
+    virtual void compile() noexcept = 0;
+    virtual void execute() noexcept = 0;
+};
+
+}// namespace vision
