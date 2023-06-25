768aaa5f24c916dd0f2352ccecf6fe564291a989
Zero <249002031@qq.com>
2023-06-26 00:19:35 +0800

    update render graph

--- src/render_graph/pass.h
+++ src/render_graph/pass.h
@@ -5,13 +5,21 @@
 #pragma once
 
 #include "rhi/common.h"
+#include "base/node.h"
+#include "resource.h"
 
 namespace vision {
 
 using namespace ocarina;
 
-class RenderPass {
+class RenderPass : public Node {
+private:
+    std::map<string, RenderResource> _input_map;
+    std::map<string, RenderResource> _output_map;
+
 public:
+    RenderPass() = default;
+
     virtual void setup() noexcept = 0;
     virtual void compile() noexcept = 0;
     virtual void execute() noexcept = 0;
--- src/render_graph/resource.h
+++ src/render_graph/resource.h
@@ -4,6 +4,7 @@
 
 #pragma once
 
+#include <utility>
 #include "rhi/common.h"
 #include "core/stl.h"
 
@@ -12,7 +13,20 @@ using namespace ocarina;
 class RenderResource {
 private:
     string _name;
+    const Type *_type{};
+    RHIResource *_rhi_resource{};
 
+public:
+    explicit RenderResource(string name = "")
+        : _name(std::move(name)) {}
+
+    template<typename T>
+    void emplace(const T &t) noexcept {
+        _rhi_resource = &t;
+        _type = Type::of<T>();
+    }
+    OC_MAKE_MEMBER_GETTER(name,)
+    OC_MAKE_MEMBER_GETTER(type,)
 };
 
 }// namespace vision
