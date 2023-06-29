87c0618b87612e3bc192c4607b0fb85f718dd520
Zero <249002031@qq.com>
2023-06-29 18:18:03 +0800

    Update integrate.cpp

--- src/render_core/passes/integrate.cpp
+++ src/render_core/passes/integrate.cpp
@@ -20,7 +20,14 @@ public:
 
     void compile() noexcept override {
         Kernel kernel = [&](Uint frame_index, BufferVar<float4> output) {
-
+            Uint2 pixel = dispatch_idx().xy();
+            sampler()->start_pixel_sample(pixel, frame_index, 0);
+            SensorSample ss = sampler()->sensor_sample(pixel, camera()->filter());
+            camera()->load_data();
+            Float scatter_pdf = 1e16f;
+            RayState rs = camera()->generate_ray(ss);
+            Float3 L = integrator()->Li(rs, scatter_pdf);
+            output.write(dispatch_id(), make_float4(L, 1.f));
         };
         _shader = device().compile(kernel, "integrate pass");
     }
@@ -35,13 +42,9 @@ public:
         return {{"radiance", "radiance", false, ResourceFormat::FLOAT4}};
     }
 
-    [[nodiscard]] static Integrator *integrator() noexcept {
-        return scene().integrator();
-    }
-
-    [[nodiscard]] static Camera *camera() noexcept {
-        return scene().camera();
-    }
+    [[nodiscard]] static Integrator *integrator() noexcept { return scene().integrator(); }
+    [[nodiscard]] static Camera *camera() noexcept { return scene().camera(); }
+    [[nodiscard]] static Sampler *sampler() noexcept { return scene().sampler(); }
 };
 
 }// namespace vision
