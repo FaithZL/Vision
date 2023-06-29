8c6fdcff71256eda0ec437771e4dfdffe1c2608e
Zero <249002031@qq.com>
2023-06-29 14:12:50 +0800

    Update integrate.cpp

--- src/render_core/passes/integrate.cpp
+++ src/render_core/passes/integrate.cpp
@@ -1,3 +1,28 @@
 //
 // Created by Zero on 2023/6/29.
 //
+
+#include "render_graph/pass.h"
+#include "base/mgr/global.h"
+#include "base/integrator.h"
+#include "base/mgr/pipeline.h"
+
+namespace vision {
+
+class IntegratePass : public RenderPass {
+private:
+    Integrator *_integrator{};
+    Shader<void(uint)> _shader;
+
+public:
+    explicit IntegratePass(const RenderPassDesc &desc)
+        : RenderPass(desc) {
+        IntegratorDesc integrator_desc;
+        integrator_desc.init({});
+        _integrator = NodeMgr::instance().load<Integrator>(integrator_desc);
+    }
+};
+
+}// namespace vision
+
+VS_MAKE_CLASS_CREATOR(vision::IntegratePass)
