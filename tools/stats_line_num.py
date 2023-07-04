da2574012c0a015abfa89192a8735a2e11eef8f5
Zero <249002031@qq.com>
2023-07-04 21:38:17 +0800

    Update stats_line_num.py

--- tools/stats_line_num.py
+++ tools/stats_line_num.py
@@ -4,10 +4,6 @@ import os
 
 count = 0
 
-com = 0
-
-inCom = False
-
 num_file = 0
 
 for root,dirs,files in os.walk(os.path.join(os.getcwd(), "src")):
@@ -39,9 +35,46 @@ for root,dirs,files in os.walk(os.path.join(os.getcwd(), "src")):
         except :
             print(fn)
 
+        num_file += 1
+
+print("renderer stats ", count, num_file)
+
+r_count = count
+r_num_file = num_file
+
+count = 0
+
+num_file = 0
+
+for root,dirs,files in os.walk(os.path.join(os.getcwd(), "src/ocarina/src")):
+    for file in files:
+        fn = os.path.join(root,file)
+        if "ext\\" in fn:
+            continue
+        if "tests" in fn:
+            continue
+        if "stats_line_num.py" in fn:
+            continue
+        if "_embed.h" in fn:
+            continue
+        if "jitify" in fn:
+            continue
+        if "sdk_pt" in fn:
+            continue
+        if ".natvis" in fn:
+            continue
+        try:
+            # print(file)
+            
+            f = open(fn, "r")
+            count += len(f.readlines())
+        except :
+            print(fn)
+
         
         
         num_file += 1
 
 
-print(count, num_file)
+print("frame work stats " , count, num_file)
+print("total stats: ", count + r_count, num_file + r_num_file)
