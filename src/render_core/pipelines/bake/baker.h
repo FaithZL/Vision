f51d815d685df7efa43d4d6520330f232747d2bb
Zero <249002031@qq.com>
2023-07-10 00:53:45 +0800

    Update baker.h

--- src/render_core/pipelines/bake/baker.h
+++ src/render_core/pipelines/bake/baker.h
@@ -10,6 +10,51 @@
 #include "batch_mesh.h"
 
 namespace vision {
+#define MAKE_ADD_TIME_FUNC(task) \
+    void add_##task##_time(double t) noexcept { task##_time += t; }
+#define VS_PLUS_TASK_TIME(task) +task##_time
+#define VS_CLEAR_TIME(task) task##_time = 0;
+#define VS_TIME_PERCENT(task) \
+    [[nodiscard]] double task##_time_percent() noexcept { return task##_time / total_time(); }
+
+#define MAKE_TIME_FUNC(...)                           \
+    MAP(MAKE_ADD_TIME_FUNC, __VA_ARGS__)              \
+    MAP(VS_TIME_PERCENT, __VA_ARGS__)                 \
+    [[nodiscard]] double total_time() noexcept {      \
+        return 0 MAP(VS_PLUS_TASK_TIME, __VA_ARGS__); \
+    }                                                 \
+    void clear_time() noexcept {                      \
+        MAP(VS_CLEAR_TIME, __VA_ARGS__)               \
+    }
+struct BakerStats {
+public:
+    uint model_num{};
+    uint64_t pixel_num{};
+    uint batch_num{};
+    uint spp{};
+    double bake_time{};
+    double raster_time{};
+    double filter_time{};
+    double package_time{};
+    double denoise_time{};
+    double uv_unwrap_time{};
+
+public:
+    MAKE_TIME_FUNC(raster, bake, filter,
+                   package, denoise,
+                   uv_unwrap)
+
+    [[nodiscard]] string get_scene_stats() const noexcept {
+        return ocarina::format("model num is {}, pixel num is {}, batch num is {}, "
+                               "{} sample per pixel, total sample num is {}",
+                               model_num, pixel_num, batch_num, spp, spp * pixel_num);
+    }
+
+};
+#undef MAKE_TIME_FUNC
+#undef VS_CLEAR_TIME
+#undef VS_PLUS_TASK_TIME
+#undef MAKE_ADD_TIME_FUNC
 
 class Baker : public Ctx {
 private:
@@ -27,8 +72,8 @@ private:
 
     /// device function
     [[nodiscard]] tuple<Float3, Float3, Bool> fetch_geometry_data(const BufferVar<Triangle> &triangles,
-                                                           const BufferVar<Vertex> &vertices,
-                                                           const BufferVar<uint4> &pixels) noexcept;
+                                                                  const BufferVar<Vertex> &vertices,
+                                                                  const BufferVar<uint4> &pixels) noexcept;
     /// device function
     [[nodiscard]] RayState generate_ray(const Float3 &position,
                                         const Float3 &normal, Float *pdf) const noexcept;
