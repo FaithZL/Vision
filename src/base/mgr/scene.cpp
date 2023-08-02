c1f2b4d108cfad190f3a6fb599e353e2453cc5fe
Zero <249002031@qq.com>
2023-08-02 13:47:36 +0800

    update scene

--- src/base/mgr/scene.cpp
+++ src/base/mgr/scene.cpp
@@ -136,37 +136,6 @@ void Scene::load_shapes(const vector<ShapeDesc> &descs) {
             _aabb.extend(instance.aabb);
             _instances.push_back(instance);
         });
-
-        //        group->for_each_mesh([&](SP<Mesh> mesh, uint i) {
-        //            auto iter = std::find_if(_materials.begin(), _materials.end(), [&](SP<Material> &material) {
-        //                return material->name() == mesh->material_name();
-        //            });
-        //            if (iter != _materials.end() && !mesh->has_material()) {
-        //                mesh->set_material(*iter);
-        //            }
-        //            if (desc.emission.valid()) {
-        //                desc.emission.set_value("inst_id", _meshes.size());
-        //                SP<IAreaLight> light = load_light<IAreaLight>(desc.emission);
-        //                mesh->set_emission(light);
-        //                light->set_mesh(mesh.get());
-        //            }
-        //            if (has_medium()) {
-        //                auto iter = std::find_if(_mediums.begin(), _mediums.end(), [&](SP<Medium> &medium) {
-        //                    return medium->name() == mesh->inside_name();
-        //                });
-        //                if (iter != _mediums.end()) {
-        //                    mesh->set_inside(*iter);
-        //                }
-        //                iter = std::find_if(_mediums.begin(), _mediums.end(), [&](SP<Medium> &medium) {
-        //                    return medium->name() == mesh->outside_name();
-        //                });
-        //                if (iter != _mediums.end()) {
-        //                    mesh->set_outside(*iter);
-        //                }
-        //            }
-        //            _aabb.extend(mesh->aabb);
-        //            _meshes.push_back(mesh);
-        //        });
     }
 }
 
