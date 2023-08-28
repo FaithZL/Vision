89c143ebab0fd74c98ebad8e249855488fb6456e
Zero <249002031@qq.com>
2023-08-28 23:50:30 +0800

    update material

---
+++ src/render_core/materials/multi_layer.cpp
@@ -0,0 +1,12 @@
+//
+// Created by Zero on 2023/8/28.
+//
+
+#include "base/scattering/material.h"
+#include "base/shader_graph/shader_node.h"
+
+namespace vision {
+
+
+
+}
--- tools/stats_line_num.py
+++ tools/stats_line_num.py
@@ -78,3 +78,8 @@ for root,dirs,files in os.walk(os.path.join(os.getcwd(), "src/ocarina/src")):
 
 print("frame work stats " , count, num_file)
 print("total stats: ", count + r_count, num_file + r_num_file)
+
+# t = 359315 + 150747 + 224500 + 72000 + 7000 + 100000 + 42500 + 200000
+
+# print(t)
+
