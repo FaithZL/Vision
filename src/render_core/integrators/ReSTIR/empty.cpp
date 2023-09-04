b8b37aaf64c4ad67f56f4d5c796c52aa8b646b45
Zero <249002031@qq.com>
2023-09-04 18:02:48 +0800

    r

---
+++ src/render_core/integrators/ReSTIR/empty.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Zero on 2023/9/4.
+//
+
+#include "reservoir.h"
--- src/render_core/integrators/ReSTIR/reservoir.h
+++ src/render_core/integrators/ReSTIR/reservoir.h
@@ -3,3 +3,22 @@
 //
 
 #pragma once
+
+#include "core/basic_types.h"
+#include "core/stl.h"
+#include "dsl/common.h"
+
+namespace vision {
+using namespace ocarina;
+
+template<EPort p = D>
+class WeightedReservoir {
+private:
+    oc_float<p> _weight_sum{};
+    oc_float<p> _value{};
+
+public:
+    
+};
+
+}
