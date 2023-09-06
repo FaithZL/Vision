3657a830cb6cee2ff03d41045be366f40fedb60c
Zero <249002031@qq.com>
2023-09-06 23:27:53 +0800

    Update reservoir.h

--- src/render_core/integrators/ReSTIR/reservoir.h
+++ src/render_core/integrators/ReSTIR/reservoir.h
@@ -11,16 +11,16 @@
 namespace vision {
 using namespace ocarina;
 
-template<typename T, EPort p = D>
+template<EPort p = D>
 class WeightedReservoir {
 private:
     oc_float<p> _weight_sum{};
-    T _value{};
+    oc_float3<p> _value{};
     oc_uint<p> _sample_num{};
 
 public:
     WeightedReservoir() = default;
-    void sample(oc_float<p> u, oc_float<p> weight, T value) {
+    void sample(oc_float<p> u, oc_float<p> weight, oc_float3<p> value) {
         _sample_num += 1;
         _weight_sum += weight;
         _value = select(u < (weight / _weight_sum), value, _value);
