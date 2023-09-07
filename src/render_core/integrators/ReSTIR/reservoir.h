91ed2a8bfd16abc3f743fc8c58cf9d93db46432d
Zero <249002031@qq.com>
2023-09-07 13:48:45 +0800

    Update reservoir.h

--- src/render_core/integrators/ReSTIR/reservoir.h
+++ src/render_core/integrators/ReSTIR/reservoir.h
@@ -16,20 +16,23 @@ public:
     static constexpr EPort p = H;
     oc_float<p> weight_sum;
     oc_float3<p> value;
+    oc_uint<p> sample_num;
 
 public:
     void update(oc_float<p> u, oc_float<p> weight, oc_float3<p> v) {
         weight_sum += weight;
+        sample_num += 1;
         value = select(u < (weight / weight_sum), v, value);
     }
 };
 
 }// namespace vision
 
-OC_STRUCT(vision::Reservoir, weight_sum, value) {
+OC_STRUCT(vision::Reservoir, weight_sum, value, sample_num) {
     static constexpr EPort p = D;
     void update(oc_float<p> u, oc_float<p> weight, oc_float3<p> v) {
         weight_sum += weight;
+        sample_num += 1;
         value = select(u < (weight / weight_sum), v, value);
     }
 };
