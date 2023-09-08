48ecdf043ed108faeafb1497fb35319f8df8d6bd
Zero <249002031@qq.com>
2023-09-08 23:51:24 +0800

    。

--- src/render_core/integrators/ReSTIR/empty.cpp
+++ src/render_core/integrators/ReSTIR/empty.cpp
@@ -2,4 +2,4 @@
 // Created by Zero on 2023/9/4.
 //
 
-#include "reservoir.h"
+#include "util.h"
--- src/render_core/integrators/ReSTIR/util.h
+++ src/render_core/integrators/ReSTIR/util.h
@@ -14,15 +14,20 @@ using namespace ocarina;
 struct Reservoir {
 public:
     static constexpr EPort p = H;
-    oc_float<p> weight_sum;
-    oc_float3<p> value;
-    oc_uint<p> sample_num;
+    oc_float<p> weight_sum{};
+    oc_float3<p> value{};
+    oc_uint<p> sample_num{};
 
 public:
     void update(oc_float<p> u, oc_float<p> weight, oc_float3<p> v) {
         weight_sum += weight;
         sample_num += 1;
         value = select(u < (weight / weight_sum), v, value);
+    }
+
+    template<typename Func>
+    [[nodiscard]] oc_float3<p> W(Func &&func) const noexcept {
+        return weight_sum / cast<float>(sample_num) / func(value);
     }
 };
 
@@ -35,4 +40,16 @@ OC_STRUCT(vision::Reservoir, weight_sum, value, sample_num) {
         sample_num += 1;
         value = select(u < (weight / weight_sum), v, value);
     }
+    template<typename Func>
+    [[nodiscard]] oc_float3<p> W(Func && func) const noexcept {
+        return weight_sum / cast<float>(sample_num) / func(value);
+    }
 };
+
+namespace vision {
+using namespace ocarina;
+
+class RIS {
+};
+
+}// namespace vision
