99906656ff4ff12be80b5762ad42f5e82bfbac54
Zero <249002031@qq.com>
2023-09-29 19:17:40 +0800

    update film

--- src/base/sensor/film.cpp
+++ src/base/sensor/film.cpp
@@ -11,6 +11,15 @@ Film::Film(const vision::FilmDesc &desc)
     : Node(desc),
       _tone_mapper(scene().load<ToneMapper>(desc.tone_mapper)),
       _resolution(desc["resolution"].as_uint2(make_uint2(1280, 720))),
-      _screen_window(make_float2(-1.f),make_float2(1.f)) {}
+      _screen_window(make_float2(-1.f), make_float2(1.f)) {
+    float ratio = _resolution.x * 1.f / _resolution.y;
+    if (ratio > 1.f) {
+        _screen_window.lower.x = -ratio;
+        _screen_window.upper.x = ratio;
+    } else {
+        _screen_window.lower.y = -1.f / ratio;
+        _screen_window.upper.y = 1.f / ratio;
+    }
+}
 
 }// namespace vision
