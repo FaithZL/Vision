627166aa3ead9e7c56888b1cd8373a11349e7978
Zero <249002031@qq.com>
2023-12-04 15:16:14 +0800

    update

--- src/base/mgr/postprocessor.cpp
+++ src/base/mgr/postprocessor.cpp
@@ -12,10 +12,10 @@ Postprocessor::Postprocessor(Pipeline *rp)
 
 void Postprocessor::compile_tone_mapping() noexcept {
     Kernel<signature> kernel = [&](BufferVar<float4> input, BufferVar<float4> output) {
-        Float4 input_pixel = input.read(thread_id());
+        Float4 input_pixel = input.read(dispatch_id());
         Float4 output_pixel = linear_to_srgb(_tone_mapper->apply(input_pixel));
         output_pixel.w = 1.f;
-        output.write(thread_id(), output_pixel);
+        output.write(dispatch_id(), output_pixel);
     };
     _tone_mapping_shader = _rp->device().compile(kernel, "tonemapping");
 }
