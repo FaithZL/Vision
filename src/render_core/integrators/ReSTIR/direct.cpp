af2c23c7e07d5d8604a57958c01081a4cecb7327
Zero <249002031@qq.com>
2023-12-15 15:01:58 +0800

    update direct

--- src/render_core/integrators/ReSTIR/direct.cpp
+++ src/render_core/integrators/ReSTIR/direct.cpp
@@ -59,7 +59,7 @@ SampledSpectrum ReSTIRDirectIllumination::Li(const Interaction &it, SampledWavel
     SampledLight sampled_light;
     sampled_light.light_index = sample.light_index;
     sampled_light.PMF = light_sampler->PMF(it, sample.light_index);
-    LightSample ls = light_sampler->sample_dir(sampled_light, it, sample.u, swl);
+    LightSample ls = light_sampler->sample_area(sampled_light, it, sample.u, swl);
     Float3 wi = normalize(ls.p_light - it.pos);
     SampledSpectrum f{swl.dimension()};
     ScatterEval eval{swl.dimension()};
