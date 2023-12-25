ab480f2a4dfc38e389d2d1270dd6e7a4142b8295
Zero <249002031@qq.com>
2023-12-25 16:11:18 +0800

    update

--- src/render_core/integrators/ReSTIR/direct.cpp
+++ src/render_core/integrators/ReSTIR/direct.cpp
@@ -164,7 +164,7 @@ DIReservoir ReSTIRDirectIllumination::RIS(Bool hit, const Interaction &it, Sampl
         LightSample ls{swl.dimension()};
         sample.p_hat = compute_p_hat(it, bsdf, swl, sample, std::addressof(ls));
         sample->set_pos(ls.p_light);
-        Float weight = Reservoir::calculate_weight(1.f / M_light, sample.p_hat, ls.eval.pdf);
+        Float weight = Reservoir::calculate_weight( (1.f) / (M_light + M_bsdf), sample.p_hat, ls.eval.pdf);
         Bool replace = ret->update(sampler->next_1d(), sample, weight);
         final_p_hat = ocarina::select(replace, sample.p_hat, final_p_hat);
     };
@@ -173,7 +173,10 @@ DIReservoir ReSTIRDirectIllumination::RIS(Bool hit, const Interaction &it, Sampl
         DIRSVSample sample;
         sample->init();
         BSDFSample bs{swl.dimension()};
-//        Float p_hat = compute_p_hat(it, bsdf, swl, addressof(sample), addressof(bs));
+        Float p_hat = compute_p_hat(it, bsdf, swl, addressof(sample), addressof(bs));
+        Float weight = Reservoir::calculate_weight((1.f) / (M_light + M_bsdf), sample.p_hat, bs.eval.pdf);
+        Bool replace = ret->update(sampler->next_1d(), sample, weight);
+        final_p_hat = ocarina::select(replace, sample.p_hat, final_p_hat);
 //        Bool replace = ret->update(sampler->next_1d(), p_hat, bs.eval.pdf, sample);
 //        final_p_hat = ocarina::select(replace, p_hat, final_p_hat);
     };
