7effab9bf4062102d7079ccd65afb84721fc9228
Zero <249002031@qq.com>
2024-01-15 18:45:32 +0800

    Update image.cpp

--- src/render_core/shadernodes/image.cpp
+++ src/render_core/shadernodes/image.cpp
@@ -11,31 +11,33 @@ namespace vision {
 using namespace ocarina;
 class Image : public ShaderNode {
 private:
-    const RegistrableTexture &_image_wrapper;
+    const RegistrableTexture &_texture;
     Serial<uint> _tex_id{};
 
 public:
     explicit Image(const ShaderNodeDesc &desc)
         : ShaderNode(desc),
-          _image_wrapper(Global::instance().pipeline()->image_pool().obtain_texture(desc)) {
-        _tex_id = _image_wrapper.index();
+          _texture(Global::instance().pipeline()->image_pool().obtain_texture(desc)) {
+        _tex_id = _texture.index();
+
+        int i = 0;
     }
-    OC_SERIALIZABLE_FUNC(ShaderNode, _tex_id)
+    OC_SERIALIZABLE_FUNC(ShaderNode, _texture)
     [[nodiscard]] string_view impl_type() const noexcept override { return VISION_PLUGIN_NAME; }
     [[nodiscard]] bool is_zero() const noexcept override { return false; }
 
     [[nodiscard]] Array<float> evaluate(const AttrEvalContext &ctx,
                                         const SampledWavelengths &swl) const noexcept override {
-        return pipeline()->tex(*_tex_id).sample(_image_wrapper.host_tex().channel_num(), ctx.uv);
+        return pipeline()->tex(*_tex_id).sample(_texture.host_tex().channel_num(), ctx.uv);
     }
     [[nodiscard]] ocarina::vector<float> average() const noexcept override {
-        return _image_wrapper.host_tex().average_vector();
+        return _texture.host_tex().average_vector();
     }
     [[nodiscard]] uint2 resolution() const noexcept override {
-        return _image_wrapper.device_tex()->resolution().xy();
+        return _texture.device_tex()->resolution().xy();
     }
     void for_each_pixel(const function<ImageIO::foreach_signature> &func) const noexcept override {
-        _image_wrapper.host_tex().for_each_pixel(func);
+        _texture.host_tex().for_each_pixel(func);
     }
 };
 }// namespace vision
