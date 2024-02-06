783c7201b338263eccbaee76ec39d3c4649d9022
Zero <249002031@qq.com>
2024-02-07 00:52:30 +0800

    Update element.h

--- src/render_core/integrators/surfel/element.h
+++ src/render_core/integrators/surfel/element.h
@@ -13,16 +13,21 @@ struct Element {
     array<float, 3> pos{};
     array<float, 3> ng{};
     float radius{};
+    uint transform_id{};
+    array<float, 3> irr{};
 };
 }// namespace vision::surfel
 
 // clang-format off
-OC_STRUCT(vision::surfel::Element, pos, ng, radius) {
+OC_STRUCT(vision::surfel::Element, pos, ng, radius, transform_id, irr) {
     [[nodiscard]] auto normal() const noexcept {
         return ng.as_vec();
     }
     [[nodiscard]] auto position() const noexcept {
         return pos.as_vec();
+    }
+    [[nodiscard]] auto irradiance() const noexcept {
+        return irr.as_vec();
     }
 };
 // clang-format on
