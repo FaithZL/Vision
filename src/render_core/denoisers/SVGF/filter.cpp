22fd9f93d2779d96752bc8b2fd712df4ef57256d
Zero <249002031@qq.com>
2024-02-08 18:55:14 +0800

    add svgf

---
+++ src/render_core/denoisers/SVGF/filter.cpp
@@ -0,0 +1,29 @@
+//
+// Created by Zero on 2024/2/8.
+//
+
+#include "core/basic_types.h"
+#include "dsl/dsl.h"
+#include "base/denoiser.h"
+
+namespace vision {
+using namespace ocarina;
+class SVGF : public Denoiser {
+
+public:
+    explicit SVGF(const DenoiserDesc &desc)
+        : Denoiser(desc) {
+    }
+    [[nodiscard]] string_view impl_type() const noexcept override { return VISION_PLUGIN_NAME; }
+    void apply(uint2 res, RegistrableManaged<float4> *output,
+               RegistrableManaged<float4> *color,
+               RegistrableManaged<float4> *normal,
+               RegistrableManaged<float4> *albedo) noexcept override {
+    }
+    void apply(uint2 res, float4 *output, float4 *color, float4 *normal, float4 *albedo) noexcept override {
+    }
+};
+
+}// namespace vision
+
+VS_MAKE_CLASS_CREATOR(vision::SVGF)
