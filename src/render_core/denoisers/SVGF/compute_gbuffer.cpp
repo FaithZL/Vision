ee7e341fa93b4c8927e18ea63b81b11ec34a3056
Zero <249002031@qq.com>
2024-02-16 23:03:00 +0800

    Update compute_gbuffer.cpp

--- src/render_core/denoisers/SVGF/compute_gbuffer.cpp
+++ src/render_core/denoisers/SVGF/compute_gbuffer.cpp
@@ -13,13 +13,50 @@ void ComputeGBuffer::prepare() noexcept {
 void ComputeGBuffer::compile() noexcept {
     Kernel kernel = [&](Uint frame_index, BufferVar<PixelData> pixel_buffer) {
         Int2 radius = make_int2(1);
-        Float normal_fwidth = 0.f;
-        Float depth_gradient = 0.f;
-        Uint sample_num = 0u;
+        Uint x_sample_num = 0u;
+        Uint y_sample_num = 0u;
+        Float3 normal_dx = make_float3(0.f);
+        Float3 normal_dy = make_float3(0.f);
+
+        Float depth_dx = 0.f;
+        Float depth_dy = 0.f;
+
+        Uint2 center = dispatch_idx().xy();
+        OCPixelData center_data = pixel_buffer.read(dispatch_id());
         for_each_neighbor(radius, [&](const Int2 &pixel) {
             Uint index = dispatch_id(pixel);
-            OCPixelData pixel_data = pixel_buffer.read(index);
+            OCPixelData neighbor_data = pixel_buffer.read(index);
+            $if(center.x > pixel.x) {
+                x_sample_num += 1;
+                normal_dx += center_data.ng.as_vec() - neighbor_data.ng.as_vec();
+                depth_dx += center_data.linear_depth - neighbor_data.linear_depth;
+            }
+            $elif(pixel.x > center.x) {
+                x_sample_num += 1;
+                normal_dx += neighbor_data.ng.as_vec() - center_data.ng.as_vec();
+                depth_dx += neighbor_data.linear_depth - center_data.linear_depth;
+            };
+
+            $if(center.y > pixel.y) {
+                y_sample_num += 1;
+                normal_dy += center_data.ng.as_vec() - neighbor_data.ng.as_vec();
+                depth_dy += center_data.linear_depth - neighbor_data.linear_depth;
+            }
+            $elif(pixel.y > center.y) {
+                y_sample_num += 1;
+                normal_dy += neighbor_data.ng.as_vec() - center_data.ng.as_vec();
+                depth_dy += neighbor_data.linear_depth - center_data.linear_depth;
+            };
         });
+        normal_dx /= cast<float>(x_sample_num);
+        normal_dy /= cast<float>(y_sample_num);
+        Float3 normal_fwidth = abs(normal_dx) + abs(normal_dy);
+        center_data.normal_fwidth = length(normal_fwidth);
+
+        depth_dx /= x_sample_num;
+        depth_dy /= y_sample_num;
+        center_data.depth_gradient = abs(depth_dx) + abs(depth_dy);
+        pixel_buffer.write(dispatch_id(), center_data);
     };
     _shader = device().compile(kernel, "SVGF-ComputeGBuffer");
 }
