3fee934fd38a8a6f28e7d7fb63ad7bf6fbe82b9b
Zero <249002031@qq.com>
2024-02-26 00:30:35 +0800

    update

---
+++ src/render_core/integrators/surfel/surfel.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Zero on 2024/2/26.
+//
+
+#include "surfel.h"
---
+++ src/render_core/integrators/surfel/surfel.h
@@ -0,0 +1,13 @@
+//
+// Created by Zero on 2024/2/26.
+//
+
+#pragma once
+
+#include "core/basic_types.h"
+#include "dsl/dsl.h"
+#include "core/stl.h"
+
+namespace vision {
+
+}
