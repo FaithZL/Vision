686dacd0e5f4309c5d662196c8ddcb53bfa9e765
Zero <249002031@qq.com>
2024-07-04 16:51:50 +0800

    Update serializer.h

--- src/hotfix/serializer.h
+++ src/hotfix/serializer.h
@@ -5,7 +5,42 @@
 #pragma once
 
 #include "math/basic_types.h"
+#include "core/stl.h"
 
 namespace vision {
 
-}
+class Serializable {
+    virtual ~Serializable() = default;
+};
+
+template<typename T>
+class SerializedData : public Serializable {
+public:
+    using data_type = T;
+
+private:
+    data_type data_;
+
+public:
+    SerializedData(const data_type &v) : data_(v) {}
+};
+
+class Serializer {
+public:
+    using group_type = std::map<ocarina::string_view, Serializable *>;
+    using map_type = std::map<ocarina::handle_ty, group_type>;
+
+private:
+    group_type group_;
+    map_type map_;
+
+public:
+    Serializer() = default;
+
+    template<typename T>
+    void serialize(ocarina::string_view name, const T &value) {
+
+    }
+};
+
+}// namespace vision
