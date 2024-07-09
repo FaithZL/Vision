8b734b250d9c13da6001c90987d609151e3d96ee
Zero <249002031@qq.com>
2024-07-09 16:13:16 +0800

    hotfix

--- src/hotfix/object_mgr.cpp
+++ src/hotfix/object_mgr.cpp
@@ -8,6 +8,37 @@ namespace vision ::inline hotfix {
 
 RuntimeObjectMgr *RuntimeObjectMgr::s_mgr = nullptr;
 
+void RuntimeObjectMgr::add_object(SP<vision::RuntimeObject> object) noexcept {
+    string c_name = object->class_name();
+    if (!map_.contains(c_name)) {
+        map_.insert(make_pair(c_name, ObjectGroup{}));
+    }
+    map_[c_name].push_back(std::move(object));
+}
+
+void RuntimeObjectMgr::remove_object(SP<vision::RuntimeObject> object) noexcept {
+    string c_name = object->class_name();
+    if (!map_.contains(c_name)) {
+        return;
+    }
+    ObjectGroup &group = map_[c_name];
+    auto iter = std::find_if(group.begin(), group.end(), [&](SP<RuntimeObject> element) {
+        return element.get() == object.get();
+    });
+    if (iter == group.end()) {
+        return;
+    }
+    group.erase(iter);
+}
+
+void RuntimeObjectMgr::update(const std::string &c_name) noexcept {
+    if (!map_.contains(c_name)) {
+        return;
+    }
+    ObjectGroup &group = map_[c_name];
+
+}
+
 RuntimeObjectMgr &RuntimeObjectMgr::instance() noexcept {
     if (s_mgr == nullptr) {
         s_mgr = new RuntimeObjectMgr();
--- src/hotfix/object_mgr.h
+++ src/hotfix/object_mgr.h
@@ -5,13 +5,17 @@
 #pragma once
 
 #include "core/stl.h"
+#include "core/hash.h"
+#include "serializer.h"
 
 namespace vision::inline hotfix {
 
 using namespace ocarina;
 
-class RuntimeObject {
-
+class RuntimeObject : public Hashable {
+public:
+    virtual void serialize(Serializer *serializer) const noexcept = 0;
+    virtual void deserialize(Serializer *serializer) const noexcept = 0;
 };
 
 class RuntimeObjectMgr {
@@ -21,13 +25,22 @@ private:
     static RuntimeObjectMgr *s_mgr;
 
 private:
-    vector<RuntimeObject> objects_;
+    using ObjectGroup = vector<SP<RuntimeObject>>;
+    map<string, ObjectGroup> map_;
+    Serializer serializer_;
 
 public:
     RuntimeObjectMgr(const RuntimeObjectMgr &) = delete;
     RuntimeObjectMgr(RuntimeObjectMgr &&) = delete;
     RuntimeObjectMgr operator=(const RuntimeObjectMgr &) = delete;
     RuntimeObjectMgr operator=(RuntimeObjectMgr &&) = delete;
+    void add_object(SP<RuntimeObject> object) noexcept;
+    void update(SP<RuntimeObject> object) noexcept {
+        update(object->class_name());
+    }
+    void update(const string &c_name) noexcept;
+    void remove_object(SP<RuntimeObject> object) noexcept;
+    OC_MAKE_MEMBER_GETTER(serializer, &)
     static RuntimeObjectMgr &instance() noexcept;
     static void destroy_instance() noexcept;
 };
