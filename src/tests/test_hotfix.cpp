464ffe728a1d011601103bffadf35b6abb93ecc2
Zero <249002031@qq.com>
2024-07-13 22:58:42 +0800

    add test

---
+++ src/tests/test_hotfix.cpp
@@ -0,0 +1,31 @@
+//
+// Created by Zero on 2024/7/31.
+//
+
+#include "hotfix/object_mgr.h"
+#include "core/stl.h"
+#include "dsl/dsl.h"
+#include "util/file_manager.h"
+#include "rhi/common.h"
+#include <windows.h>
+#include "math/base.h"
+#include "util/image.h"
+#include "dsl/dsl.h"
+#include "GUI_impl/imGui/window.h"
+#include "util/image.h"
+
+using namespace ocarina;
+
+int main(int argc, char *argv[]) {
+
+    fs::path path(argv[0]);
+    FileManager file_manager(path.parent_path());
+
+    auto window = file_manager.create_window("display", make_uint2(500), "imGui");
+    auto image_io = Image::pure_color(make_float4(1,0,0,1), ColorSpace::LINEAR, make_uint2(500));
+    window->run([&](double d){
+        window->set_background(image_io.pixel_ptr<float4>(), make_uint2(500));
+    });
+
+    return 0;
+}
