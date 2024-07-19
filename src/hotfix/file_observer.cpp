c4c7d496792fcf9620533fa322f463392263a813
Zero <249002031@qq.com>
2024-07-19 12:12:24 +0800

    add file observer

--- src/hotfix/file_observer.cpp
+++ src/hotfix/file_observer.cpp
@@ -6,4 +6,18 @@
 
 namespace vision::inline hotfix {
 
+void FileObserver::add_observed(const fs::path &path, bool recursive) noexcept {
+    ObservedFile observed;
+    observed.path = path;
+    observed.recursive = recursive;
+    group_.insert(std::make_pair(observed.path.string(), observed));
 }
+
+void FileObserver::remove_observed(const fs::path &path) noexcept {
+    string key = path.string();
+    if (group_.contains(key)) {
+        group_.erase(key);
+    }
+}
+
+}// namespace vision::inline hotfix
--- src/hotfix/file_observer.h
+++ src/hotfix/file_observer.h
@@ -7,5 +7,32 @@
 #include "core/stl.h"
 
 namespace vision::inline hotfix {
+using namespace ocarina;
+class FileObserver {
+public:
+    enum Action {
+        Add = 1,
+        Delete = 1 << 1,
+        Modify = 1 << 2,
+    };
 
-}
+    struct ObservedFile {
+    public:
+        ocarina::fs::path path{};
+        bool recursive{};
+        Action action{Modify};
+        uint32_t change_time{};
+    };
+
+    using group_type = map<string, ObservedFile>;
+
+private:
+    group_type group_;
+
+public:
+    FileObserver() = default;
+    void add_observed(const fs::path &path, bool recursive = false) noexcept;
+    void remove_observed(const fs::path &path) noexcept;
+};
+
+}// namespace vision::inline hotfix
