569cb06b8160f65553d4365c22eab4833106fbbf
Zero <249002031@qq.com>
2024-08-07 21:00:56 +0800

    Update cmd_process.h

--- src/hotfix/platform_win/cmd_process.h
+++ src/hotfix/platform_win/cmd_process.h
@@ -31,97 +31,92 @@ struct CmdProcess {
 };
 
 CmdProcess::CmdProcess()
-    : m_CmdProcessOutputRead(NULL), m_CmdProcessInputWrite(NULL),
+    : m_CmdProcessOutputRead(nullptr), m_CmdProcessInputWrite(nullptr),
       m_bIsComplete(false), m_bStoreCmdOutput(false) {
     ZeroMemory(&m_CmdProcessInfo, sizeof(m_CmdProcessInfo));
 }
 
-
-void ReadAndHandleOutputThread( LPVOID arg )
-{
-    CmdProcess* pCmdProc = (CmdProcess*)arg;
+void ReadAndHandleOutputThread(LPVOID arg) {
+    CmdProcess *pCmdProc = (CmdProcess *)arg;
 
     CHAR lpBuffer[1024];
     DWORD nBytesRead;
     bool bReadActive = true;
-    while( bReadActive )
-    {
-        if( !ReadFile( pCmdProc->m_CmdProcessOutputRead,lpBuffer,sizeof(lpBuffer)-1,
-                      &nBytesRead,NULL) || !nBytesRead)
-        {
+    while (bReadActive) {
+        if (!ReadFile(pCmdProc->m_CmdProcessOutputRead, lpBuffer, sizeof(lpBuffer) - 1,
+                      &nBytesRead, nullptr) ||
+            !nBytesRead) {
             bReadActive = false;
-            if( GetLastError() != ERROR_BROKEN_PIPE)	//broken pipe is OK
+            if (GetLastError() != ERROR_BROKEN_PIPE)//broken pipe is OK
             {
-                OC_WARNING( "[RuntimeCompiler] Redirect of compile output failed on read\n" );
+                OC_WARNING("[RuntimeCompiler] Redirect of compile output failed on read\n");
             }
-        }
-        else
-        {
+        } else {
             // Add null termination
-            lpBuffer[nBytesRead]=0;
+            lpBuffer[nBytesRead] = 0;
             //fist check for completion token...
-            std::string buffer( lpBuffer );
-            size_t found = buffer.find( c_CompletionToken );
-            if( found != std::string::npos )
-            {
+            std::string buffer(lpBuffer);
+            size_t found = buffer.find(c_CompletionToken);
+            if (found != std::string::npos) {
                 //we've found the completion token, which means we quit
-                buffer = buffer.substr( 0, found );
-                if( !pCmdProc->m_bStoreCmdOutput ) {
+                buffer = buffer.substr(0, found);
+                if (!pCmdProc->m_bStoreCmdOutput) {
                     OC_INFO("[RuntimeCompiler] Complete\n");
                 }
                 pCmdProc->m_bIsComplete = true;
             }
 
-            if( bReadActive || buffer.length() ) //don't output blank last line
-            {
-                if( pCmdProc->m_bStoreCmdOutput )
-                {
+            if (bReadActive || buffer.length()) {
+                //don't output blank last line
+                if (pCmdProc->m_bStoreCmdOutput) {
                     pCmdProc->m_CmdOutput += buffer;
-                }
-                else
-                {
+                } else {
                     //check if this is an error
-                    size_t errorFound = buffer.find( " : error " );
-                    size_t fatalErrorFound = buffer.find( " : fatal error " );
-                    if( ( errorFound != std::string::npos ) || ( fatalErrorFound != std::string::npos ) )
-                    {
-                        OC_WARNING_FORMAT( "{}", buffer.c_str() );
-                    }
-                    else
-                    {
-                        OC_INFO_FORMAT( "{}", buffer.c_str() );
+                    size_t errorFound = buffer.find(" : error ");
+                    size_t fatalErrorFound = buffer.find(" : fatal error ");
+                    if ((errorFound != std::string::npos) || (fatalErrorFound != std::string::npos)) {
+                        OC_WARNING_FORMAT("{}", buffer.c_str());
+                    } else {
+                        OC_INFO_FORMAT("{}", buffer.c_str());
                     }
                 }
             }
-
         }
     }
-
 }
 
-void CmdProcess::InitialiseProcess()
-{
+void CmdProcess::InitialiseProcess() {
     //init compile process
-    STARTUPINFOW				si;
+    STARTUPINFOW si;
     ZeroMemory(&si, sizeof(si));
     si.cb = sizeof(si);
 
     // Set up the security attributes struct.
     SECURITY_ATTRIBUTES sa;
     sa.nLength = sizeof(SECURITY_ATTRIBUTES);
-    sa.lpSecurityDescriptor = NULL;
+    sa.lpSecurityDescriptor = nullptr;
     sa.bInheritHandle = TRUE;
-
 
     // Create the child output pipe.
     //redirection of output
     si.dwFlags = STARTF_USESTDHANDLES | STARTF_USESHOWWINDOW;
     si.wShowWindow = SW_HIDE;
-    HANDLE hOutputReadTmp = NULL, hOutputWrite = NULL, hErrorWrite = NULL;
-    if (!CreatePipe(&hOutputReadTmp, &hOutputWrite, &sa, 20 * 1024))
-    {
+    HANDLE hOutputReadTmp = nullptr, hOutputWrite = nullptr, hErrorWrite = nullptr;
+    auto exit_func = [&] {
+        if (hOutputReadTmp) {
+            CloseHandle(hOutputReadTmp);
+        }
+        if (hOutputWrite) {
+            CloseHandle(hOutputWrite);
+        }
+        if (hErrorWrite) {
+            CloseHandle(hErrorWrite);
+        }
+    };
+    if (!CreatePipe(&hOutputReadTmp, &hOutputWrite, &sa, 20 * 1024)) {
         OC_WARNING("[RuntimeCompiler] Failed to create output redirection pipe\n");
-//        goto ERROR_EXIT;
+        exit_func();
+        return;
     }
     si.hStdOutput = hOutputWrite;
 
@@ -130,40 +125,37 @@ void CmdProcess::InitialiseProcess()
     // closes one of its std output handles.
     if (!DuplicateHandle(GetCurrentProcess(), hOutputWrite,
                          GetCurrentProcess(), &hErrorWrite, 0,
-                         TRUE, DUPLICATE_SAME_ACCESS))
-    {
+                         TRUE, DUPLICATE_SAME_ACCESS)) {
         OC_WARNING("[RuntimeCompiler] Failed to duplicate error output redirection pipe\n");
-//        goto ERROR_EXIT;
+        exit_func();
+        return;
     }
     si.hStdError = hErrorWrite;
-
 
     // Create new output read handle and the input write handles. Set
     // the Properties to FALSE. Otherwise, the child inherits the
     // properties and, as a result, non-closeable handles to the pipes
     // are created.
-    if (si.hStdOutput)
-    {
+    if (si.hStdOutput) {
         if (!DuplicateHandle(GetCurrentProcess(), hOutputReadTmp,
                              GetCurrentProcess(),
-                             &m_CmdProcessOutputRead, // Address of new handle.
-                             0, FALSE, // Make it uninheritable.
-                             DUPLICATE_SAME_ACCESS))
-        {
+                             &m_CmdProcessOutputRead,// Address of new handle.
+                             0, FALSE,               // Make it uninheritable.
+                             DUPLICATE_SAME_ACCESS)) {
             OC_WARNING("[RuntimeCompiler] Failed to duplicate output read pipe\n");
-//            goto ERROR_EXIT;
+            exit_func();
+            return;
         }
         CloseHandle(hOutputReadTmp);
-        hOutputReadTmp = NULL;
+        hOutputReadTmp = nullptr;
     }
-
 
     HANDLE hInputRead, hInputWriteTmp;
     // Create a pipe for the child process's STDIN.
-    if (!CreatePipe(&hInputRead, &hInputWriteTmp, &sa, 4096))
-    {
+    if (!CreatePipe(&hInputRead, &hInputWriteTmp, &sa, 4096)) {
         OC_WARNING("[RuntimeCompiler] Failed to create input pipes\n");
-//        goto ERROR_EXIT;
+        exit_func();
+        return;
     }
     si.hStdInput = hInputRead;
 
@@ -171,75 +163,53 @@ void CmdProcess::InitialiseProcess()
     // the Properties to FALSE. Otherwise, the child inherits the
     // properties and, as a result, non-closeable handles to the pipes
     // are created.
-    if (si.hStdOutput)
-    {
-        if (!DuplicateHandle(GetCurrentProcess(), hInputWriteTmp,
-                             GetCurrentProcess(),
-                             &m_CmdProcessInputWrite, // Address of new handle.
-                             0, FALSE, // Make it uninheritable.
-                             DUPLICATE_SAME_ACCESS))
-        {
-            OC_WARNING("[RuntimeCompiler] Failed to duplicate input write pipe\n");
-//            goto ERROR_EXIT;
-        }
+    if (si.hStdOutput && !DuplicateHandle(GetCurrentProcess(), hInputWriteTmp,
+                                          GetCurrentProcess(),
+                                          &m_CmdProcessInputWrite,// Address of new handle.
+                                          0, FALSE,               // Make it uninheritable.
+                                          DUPLICATE_SAME_ACCESS)) {
+        OC_WARNING("[RuntimeCompiler] Failed to duplicate input write pipe\n");
+        exit_func();
+        return;
     }
 
-
-    const wchar_t* pCommandLine = L"cmd /q /K @PROMPT $";
+    const wchar_t *pCommandLine = L"cmd /q /K @PROMPT $";
     //CreateProcessW won't accept a const pointer, so copy to an array
     wchar_t pCmdLineNonConst[1024];
     wcscpy_s(pCmdLineNonConst, pCommandLine);
     CreateProcessW(
-        NULL,				//__in_opt     LPCTSTR lpApplicationName,
-        pCmdLineNonConst,			//__inout_opt  LPTSTR lpCommandLine,
-        NULL,				//__in_opt     LPSECURITY_ATTRIBUTES lpProcessAttributes,
-        NULL,				//__in_opt     LPSECURITY_ATTRIBUTES lpThreadAttributes,
-        TRUE,				//__in         BOOL bInheritHandles,
-        0,				//__in         DWORD dwCreationFlags,
-        NULL,				//__in_opt     LPVOID lpEnvironment,
-        NULL,				//__in_opt     LPCTSTR lpCurrentDirectory,
-        &si,				//__in         LPSTARTUPINFO lpStartupInfo,
-        &m_CmdProcessInfo				//__out        LPPROCESS_INFORMATION lpProcessInformation
+        nullptr,         //__in_opt     LPCTSTR lpApplicationName,
+        pCmdLineNonConst,//__inout_opt  LPTSTR lpCommandLine,
+        nullptr,         //__in_opt     LPSECURITY_ATTRIBUTES lpProcessAttributes,
+        nullptr,         //__in_opt     LPSECURITY_ATTRIBUTES lpThreadAttributes,
+        TRUE,            //__in         BOOL bInheritHandles,
+        0,               //__in         DWORD dwCreationFlags,
+        nullptr,         //__in_opt     LPVOID lpEnvironment,
+        nullptr,         //__in_opt     LPCTSTR lpCurrentDirectory,
+        &si,             //__in         LPSTARTUPINFO lpStartupInfo,
+        &m_CmdProcessInfo//__out        LPPROCESS_INFORMATION lpProcessInformation
     );
 
     //launch threaded read.
-    _beginthread(ReadAndHandleOutputThread, 0, this); //this will exit when process for compile is closed
+    _beginthread(ReadAndHandleOutputThread, 0, this);//this will exit when process for compile is closed
 
-
-ERROR_EXIT:
-    if( hOutputReadTmp )
-    {
-        CloseHandle( hOutputReadTmp );
-    }
-    if( hOutputWrite )
-    {
-        CloseHandle(hOutputWrite);
-    }
-    if( hErrorWrite )
-    {
-        CloseHandle( hErrorWrite );
-    }
+    exit_func();
 }
 
-
-void CmdProcess::WriteInput( std::string& input )
-{
+void CmdProcess::WriteInput(std::string &input) {
     DWORD nBytesWritten;
     DWORD length = (DWORD)input.length();
-    WriteFile( m_CmdProcessInputWrite , input.c_str(), length, &nBytesWritten, NULL);
+    WriteFile(m_CmdProcessInputWrite, input.c_str(), length, &nBytesWritten, nullptr);
 }
 
-void CmdProcess::CleanupProcessAndPipes()
-{
+void CmdProcess::CleanupProcessAndPipes() {
     // do not reset m_bIsComplete and other members here, just process and pipes and
-    if( !m_PathTempCLCommandFile.empty() && fs::exists(m_PathTempCLCommandFile) )
-    {
+    if (!m_PathTempCLCommandFile.empty() && fs::exists(m_PathTempCLCommandFile)) {
         fs::remove(m_PathTempCLCommandFile);
         m_PathTempCLCommandFile.clear();
     }
 
-    if( m_CmdProcessInfo.hProcess )
-    {
+    if (m_CmdProcessInfo.hProcess) {
         TerminateProcess(m_CmdProcessInfo.hProcess, 0);
         TerminateThread(m_CmdProcessInfo.hThread, 0);
         CloseHandle(m_CmdProcessInfo.hThread);
@@ -251,10 +221,8 @@ void CmdProcess::CleanupProcessAndPipes()
     }
 }
 
-CmdProcess::~CmdProcess()
-{
+CmdProcess::~CmdProcess() {
     CleanupProcessAndPipes();
 }
-
 
 }// namespace vision::inline hotfix
