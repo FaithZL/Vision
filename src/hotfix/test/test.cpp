ec4308ca19eebc9a724dee39d527ce3a0f457503
Zero <249002031@qq.com>
2024-08-17 00:27:22 +0800

    ，

---
+++ src/hotfix/test/test.cpp
@@ -0,0 +1,9 @@
+//
+// Created by Zero on 2024/8/17.
+//
+
+#include "test.h"
+
+namespace vision ::inline hotfix {
+
+}
---
+++ src/hotfix/test/test.h
@@ -0,0 +1,15 @@
+//
+// Created by Zero on 2024/8/17.
+//
+
+#pragma once
+
+#include "core/stl.h"
+#include "hotfix/object.h"
+
+namespace vision::inline hotfix {
+
+struct Test : RuntimeObject {
+};
+
+}// namespace vision::inline hotfix
