389d060b64b0e58d55b77c3477360710949f3a7a
Zero <249002031@qq.com>
2024-08-19 23:55:25 +0800

    update

--- src/hotfix/rules_parsers/ninja.cpp
+++ src/hotfix/rules_parsers/ninja.cpp
@@ -27,8 +27,21 @@ void NinjaParser::process_lines(std::string_view *lines) {
         return line.substr(size_build_token, index + size_obj_token - size_build_token);
     };
 
+    auto find_src = [&]{
+        constexpr auto compiler_token = "CXX_COMPILER__";
+        auto index = line.find(compiler_token);
+        uint start = index + strlen(compiler_token);
+        auto space_index = line.find(' ', start);
+        constexpr auto next_token = "||";
+        auto next_token_index = line.find(next_token, space_index);
+        auto ret = string(line.substr(space_index + 1, next_token_index - space_index - 2));
+        ret = replace(ret, "$", "");
+        return ret;
+    };
+
     CompileOptions options;
     options.dst = find_dst();
+    options.src = find_src();
     compiles_.push_back(options);
 }
 
