c7593509aedbf1fa48cb9ad5cf29d82524c7d980
Zero <249002031@qq.com>
2024-08-21 17:33:46 +0800

    。

---
+++ src/hotfix/test/demo.cpp
@@ -0,0 +1,19 @@
+//
+// Created by Zero on 2024/8/21.
+//
+
+#include "demo.h"
+
+namespace vision::inline hotfix {
+
+IObjectConstructor *Demo::constructor() const noexcept {
+    return nullptr;
+}
+
+void Demo::serialize(vision::Serializer *serializer) const noexcept {
+}
+
+void Demo::deserialize(vision::Serializer *serializer) const noexcept {
+}
+
+}// namespace vision::inline hotfix
---
+++ src/hotfix/test/demo.h
@@ -0,0 +1,19 @@
+//
+// Created by Zero on 2024/8/21.
+//
+
+#pragma once
+
+#include "core/stl.h"
+#include "hotfix/object.h"
+#include "hotfix/hotfix.h"
+
+namespace vision::inline hotfix {
+
+struct Demo : RuntimeObject {
+    void serialize(vision::Serializer *serializer) const noexcept override;
+    void deserialize(vision::Serializer *serializer) const noexcept override;
+    [[nodiscard]] IObjectConstructor *constructor() const noexcept override;
+};
+
+}// namespace vision::inline hotfix
