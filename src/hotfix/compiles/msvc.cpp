dac365444132fe155efbb952a26f606438ad880c
Zero <249002031@qq.com>
2024-08-23 12:41:18 +0800

    Update msvc.cpp

--- src/hotfix/compiles/msvc.cpp
+++ src/hotfix/compiles/msvc.cpp
@@ -58,16 +58,12 @@ public:
         cmd_process_.write_input(cmd);
     }
 
-    [[nodiscard]] static vector<string> assemble_link_cmds(const LinkOptions &options, const FileInspector::Target &target) noexcept;
+    [[nodiscard]] static string assemble_link_cmds(const LinkOptions &options, const FileInspector::Target &target) noexcept;
     void link(const vision::LinkOptions &options, const FileInspector::Target &target) noexcept override;
 };
 
-vector<string> MSVCompiler::assemble_link_cmds(const LinkOptions &options,
+string MSVCompiler::assemble_link_cmds(const LinkOptions &options,
                                       const FileInspector::Target &target) noexcept {
-    /// pre link
-    vector<string> ret;
-    auto ccc = "link src\\hotfix\\test\\CMakeFiles\\vision-hotfix-test.dir\\demo.cpp.obj src\\hotfix\\test\\CMakeFiles\\vision-hotfix-test.dir\\test.cpp.obj  /out:bin\\vision-hotfix-test333.dll /implib:lib\\vision-hotfix-test.lib /pdb:bin\\vision-hotfix-test.pdb /dll /version:0.0 /machine:x64 /debug /INCREMENTAL  /DEF:src\\hotfix\\test\\CMakeFiles\\vision-hotfix-test.dir\\.\\exports.def  lib\\vision-hotfix.lib  lib\\ocarina.lib  lib\\ocarina-dsl.lib  lib\\ocarina-rhi.lib  lib\\ocarina-GUI.lib  lib\\ocarina-generator.lib  lib\\ocarina-ast.lib  lib\\ocarina-util.lib  lib\\ocarina-core.lib  lib\\ocarina-math.lib  lib\\ocarina-ext.lib  lib\\EASTL.lib  lib\\spdlogd.lib  lib\\ocarina-imgui.lib  lib\\glfw3.lib  lib\\ocarina-ext-stb.lib  lib\\ocarina-ext-tinyexr.lib  dbghelp.lib  kernel32.lib user32.lib gdi32.lib winspool.lib shell32.lib ole32.lib oleaut32.lib uuid.lib comdlg32.lib advapi32.lib";
-
     /// obj out implib pdb link_flags dependency
     static constexpr string_view cmd_template = "link {} /out:{} /implib:{} /pdb:{} /dll {} {}";
     string link_cmd = ocarina::format(cmd_template, options.obj_files_string(),
@@ -76,19 +72,13 @@ vector<string> MSVCompiler::assemble_link_cmds(const LinkOptions &options,
                                       "bin\\vision-hotfix-test567.pdb",
                                       options.link_flags,
                                       options.link_libraries);
-
-    string cmd = options.pre_link + "&& " + link_cmd;
-    ret.push_back(cmd);
-
-    return ret;
+    return link_cmd;
 }
 
 void MSVCompiler::link(const vision::LinkOptions &options,
                        const FileInspector::Target &target) noexcept {
-    auto cmds = assemble_link_cmds(options, target);
-    for (const auto &item : cmds) {
-        cmd_process_.write_input(item);
-    }
+    auto cmd = assemble_link_cmds(options, target);
+    cmd_process_.write_input(cmd);
 }
 
 }// namespace vision::inline hotfix
