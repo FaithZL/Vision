36076e177f67e09b9e33ffe9cbb736266aae4b05
Zero <249002031@qq.com>
2024-10-15 14:42:02 +0800

    Update inspector.h

--- src/render_core/integrators/adaptive/inspector.h
+++ src/render_core/integrators/adaptive/inspector.h
@@ -6,11 +6,31 @@
 
 #include "core/stl.h"
 #include "hotfix/hotfix.h"
+#include "dsl/dsl.h"
+#include "UI/GUI.h"
+
+namespace vision {
+struct alignas(16u) VarianceStats {
+    float average{};
+    float variance{};
+    uint count{};
+
+    void add(float value) noexcept {
+        average = (average * count + value) / (count + 1);
+        count += 1;
+    }
+};
+}// namespace vision
+
+// clang-format off
+OC_STRUCT(vision, VarianceStats, average, variance, count) {};
+// clang-format on
 
 namespace vision {
 
-class ConvergenceInspector : public RuntimeObject {
+class ConvergenceInspector : public GUI, public RuntimeObject {
 private:
+
 };
 
 }// namespace vision
