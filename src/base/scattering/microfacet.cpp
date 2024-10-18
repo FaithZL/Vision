4e25df629b380eb0f1d380cc363f0ea600933234
Zero <249002031@qq.com>
2024-10-18 23:37:08 +0800

    update microfacet

--- src/base/scattering/microfacet.cpp
+++ src/base/scattering/microfacet.cpp
@@ -6,7 +6,127 @@
 
 namespace vision {
 
-namespace microfacet {
+inline namespace microfacet {
+template<EPort p>
+[[nodiscard]] oc_float<p> D_(const oc_float3<p> &wh, oc_float<p> alpha_x, oc_float<p> alpha_y, MicrofacetType type) {
+    // When theta is close to 90, tan theta is infinity
+    oc_float<p> tan_theta_2 = geometry::tan_theta_2(wh);
+    oc_float<p> cos_theta_4 = sqr(geometry::cos_theta_2(wh));
+    switch (type) {
+        case Disney:
+        case GGX: {
+            oc_float<p> e = tan_theta_2 * (sqr(geometry::cos_phi(wh) / alpha_x) + sqr(geometry::sin_phi(wh) / alpha_y));
+            oc_float<p> ret = 1.f / (Pi * alpha_x * alpha_y * cos_theta_4 * sqr(1 + e));
+            return select(cos_theta_4 < 1e-16f || ocarina::isinf(tan_theta_2), 0.f, ret);
+        }
+        case Beckmann: {
+            oc_float<p> ret = exp(-tan_theta_2 * (geometry::cos_phi_2(wh) / sqr(alpha_x) +
+                                                  geometry::sin_phi_2(wh) / sqr(alpha_y))) /
+                              (Pi * alpha_x * alpha_y * cos_theta_4);
+            return select(cos_theta_4 < 1e-16f || ocarina::isinf(tan_theta_2), 0.f, ret);
+        }
+        default:
+            break;
+    }
+    return 0;
+}
+template oc_float<D> D_<D>(const oc_float3<D> &wh, oc_float<D> alpha_x, oc_float<D> alpha_y, MicrofacetType type);
+
+template<EPort p>
+[[nodiscard]] oc_float<p> lambda(const oc_float3<p> &w, const oc_float<p> &alpha_x,
+                                 const oc_float<p> &alpha_y, MicrofacetType type) {
+    switch (type) {
+        case Disney:
+        case GGX: {
+            oc_float<p> abs_tan_theta = abs(geometry::tan_theta(w));
+            oc_float<p> cos_theta2 = geometry::cos_theta_2(w);
+            oc_float<p> sin_theta2 = geometry::sin_theta_2(w);
+            oc_float<p> alpha = sqrt(cos_theta2 * sqr(alpha_x) +
+                                     sin_theta2 * sqr(alpha_y));
+            oc_float<p> ret = (-1 + sqrt(1.f + sqr(alpha * abs_tan_theta))) / 2;
+            return select(ocarina::isinf(abs_tan_theta), 0.f, ret);
+        }
+        case Beckmann: {
+            oc_float<p> abs_tan_theta = abs(geometry::tan_theta(w));
+
+            oc_float<p> cos_theta2 = geometry::cos_theta_2(w);
+            oc_float<p> sin_theta2 = geometry::sin_theta_2(w);
+
+            oc_float<p> alpha = sqrt(cos_theta2 * sqr(alpha_x) +
+                                     sin_theta2 * sqr(alpha_y));
+            oc_float<p> a = 1.f / (alpha * abs_tan_theta);
+
+            oc_float<p> ret = (1 - 1.259f * a + 0.396f * sqr(a)) / (3.535f * a + 2.181f * sqr(a));
+            return select(a >= 1.6f || ocarina::isinf(abs_tan_theta), 0.f, ret);
+        }
+        default:
+            break;
+    }
+    return 0;
+}
+template oc_float<D> lambda<D>(const oc_float3<D> &w, const oc_float<D> &alpha_x,
+                               const oc_float<D> &alpha_y, MicrofacetType type);
+
+template<EPort p>
+[[nodiscard]] oc_float3<p> sample_wh(const oc_float3<p> &wo, const oc_float2<p> &u,
+                                     oc_float<p> alpha_x, oc_float<p> alpha_y, MicrofacetType type) {
+    switch (type) {
+        case Disney:
+        case GGX: {
+            oc_float<p> cos_theta = 0, phi = _2Pi * u[1];
+            phi = atan(alpha_y / alpha_x * tan(_2Pi * u[1] + PiOver2));
+            phi = select(u[1] > .5f, phi + Pi, phi);
+            oc_float<p> sin_phi = sin(phi), cos_phi = cos(phi);
+            oc_float<p> alpha2 = 1.f / (sqr(cos_phi / alpha_x) + sqr(sin_phi / alpha_y));
+            oc_float<p> tan_theta_2 = alpha2 * u[0] / (1 - u[0]);
+            cos_theta = 1 / sqrt(1 + tan_theta_2);
+            oc_float<p> sin_theta = safe_sqrt(1 - sqr(cos_theta));
+            oc_float3<p> wh = spherical_direction<p>(sin_theta, cos_theta, phi);
+            wh = select(same_hemisphere(wo, wh), wh, -wh);
+            //            CHECK_UNIT_VEC(wh)
+            return wh;
+        }
+        case Beckmann: {
+            oc_float<p> tan_theta_2, phi;
+            oc_float<p> log_sample = log(1 - u[0]);
+            phi = atan(alpha_y / alpha_x *
+                       tan(_2Pi * u[1] + PiOver2));
+            phi = select(u[1] > .5f, phi + Pi, phi);
+            oc_float<p> sin_phi = sin(phi), cos_phi = cos(phi);
+            tan_theta_2 = -log_sample / (sqr(cos_phi / alpha_x) + sqr(sin_phi / alpha_y));
+            oc_float<p> cos_theta = 1 / sqrt(1 + tan_theta_2);
+            oc_float<p> sin_theta = safe_sqrt(1 - sqr(cos_theta));
+            oc_float3<p> wh = spherical_direction<p>(sin_theta, cos_theta, phi);
+            wh = select(same_hemisphere(wo, wh), wh, -wh);
+            //            CHECK_UNIT_VEC(wh)
+            return wh;
+        }
+        default:
+            break;
+    }
+    return {};
+}
+template oc_float3<D> sample_wh<D>(const oc_float3<D> &wo, const oc_float2<D> &u,
+                                   oc_float<D> alpha_x, oc_float<D> alpha_y, MicrofacetType type);
+
+template<EPort p>
+[[nodiscard]] oc_float<p> BTDF_div_ft(const oc_float3<p> &wo, const oc_float3<p> &wh, const oc_float3<p> &wi,
+                                      const oc_float<p> &eta, const oc_float<p> &alpha_x,
+                                      const oc_float<p> &alpha_y, MicrofacetType type) {
+    oc_float<p> cos_theta_i = cos_theta(wi);
+    oc_float<p> cos_theta_o = cos_theta(wo);
+    oc_float<p> numerator = D_<p>(wh, alpha_x, alpha_y, type) * G_<p>(wo, wi, alpha_x, alpha_y, type) *
+                            abs(dot(wi, wh) * dot(wo, wh));
+    oc_float<p> denom = sqr(dot(wi, wh) * eta + dot(wo, wh)) * abs(cos_theta_i * cos_theta_o);
+    oc_float<p> ft = numerator / denom;
+    oc_float<p> factor = rcp(sqr(eta));
+    return ft * factor;
+}
+
+template oc_float<D> BTDF_div_ft<D>(const oc_float3<D> &wo, const oc_float3<D> &wh, const oc_float3<D> &wi,
+                                    const oc_float<D> &eta, const oc_float<D> &alpha_x,
+                                    const oc_float<D> &alpha_y, MicrofacetType type);
+
 [[nodiscard]] Float BTDF_div_ft(const Float3 &wo, const Float3 &wh, const Float3 &wi,
                                 const Float &eta, const Float &alpha_x,
                                 const Float &alpha_y, MicrofacetType type) {
@@ -19,6 +139,7 @@ namespace microfacet {
     Float factor = rcp(sqr(eta));
     return ft * factor;
 }
+
 }// namespace microfacet
 
 Float GGXMicrofacet::D_(Float3 wh) const noexcept {
--- src/base/scattering/microfacet.h
+++ src/base/scattering/microfacet.h
@@ -59,28 +59,7 @@ template<EPort p = D>
  * @return
  */
 template<EPort p = D>
-[[nodiscard]] oc_float<p> D_(const oc_float3<p> &wh, oc_float<p> alpha_x, oc_float<p> alpha_y, MicrofacetType type = GGX) {
-    // When theta is close to 90, tan theta is infinity
-    oc_float<p> tan_theta_2 = geometry::tan_theta_2(wh);
-    oc_float<p> cos_theta_4 = sqr(geometry::cos_theta_2(wh));
-    switch (type) {
-        case Disney:
-        case GGX: {
-            oc_float<p> e = tan_theta_2 * (sqr(geometry::cos_phi(wh) / alpha_x) + sqr(geometry::sin_phi(wh) / alpha_y));
-            oc_float<p> ret = 1.f / (Pi * alpha_x * alpha_y * cos_theta_4 * sqr(1 + e));
-            return select(cos_theta_4 < 1e-16f || ocarina::isinf(tan_theta_2), 0.f, ret);
-        }
-        case Beckmann: {
-            oc_float<p> ret = exp(-tan_theta_2 * (geometry::cos_phi_2(wh) / sqr(alpha_x) +
-                                                  geometry::sin_phi_2(wh) / sqr(alpha_y))) /
-                              (Pi * alpha_x * alpha_y * cos_theta_4);
-            return select(cos_theta_4 < 1e-16f || ocarina::isinf(tan_theta_2), 0.f, ret);
-        }
-        default:
-            break;
-    }
-    return 0;
-}
+[[nodiscard]] oc_float<p> D_(const oc_float3<p> &wh, oc_float<p> alpha_x, oc_float<p> alpha_y, MicrofacetType type = GGX);
 
 /**
  * lambda(w) = A-(w) / (A+(w) - A-(w))
@@ -89,36 +68,7 @@ template<EPort p = D>
  */
 template<EPort p = EPort::D>
 [[nodiscard]] oc_float<p> lambda(const oc_float3<p> &w, const oc_float<p> &alpha_x,
-                                 const oc_float<p> &alpha_y, MicrofacetType type = GGX) {
-    switch (type) {
-        case Disney:
-        case GGX: {
-            oc_float<p> abs_tan_theta = abs(geometry::tan_theta(w));
-            oc_float<p> cos_theta2 = geometry::cos_theta_2(w);
-            oc_float<p> sin_theta2 = geometry::sin_theta_2(w);
-            oc_float<p> alpha = sqrt(cos_theta2 * sqr(alpha_x) +
-                                     sin_theta2 * sqr(alpha_y));
-            oc_float<p> ret = (-1 + sqrt(1.f + sqr(alpha * abs_tan_theta))) / 2;
-            return select(ocarina::isinf(abs_tan_theta), 0.f, ret);
-        }
-        case Beckmann: {
-            oc_float<p> abs_tan_theta = abs(geometry::tan_theta(w));
-
-            oc_float<p> cos_theta2 = geometry::cos_theta_2(w);
-            oc_float<p> sin_theta2 = geometry::sin_theta_2(w);
-
-            oc_float<p> alpha = sqrt(cos_theta2 * sqr(alpha_x) +
-                                     sin_theta2 * sqr(alpha_y));
-            oc_float<p> a = 1.f / (alpha * abs_tan_theta);
-
-            oc_float<p> ret = (1 - 1.259f * a + 0.396f * sqr(a)) / (3.535f * a + 2.181f * sqr(a));
-            return select(a >= 1.6f || ocarina::isinf(abs_tan_theta), 0.f, ret);
-        }
-        default:
-            break;
-    }
-    return 0;
-}
+                                 const oc_float<p> &alpha_y, MicrofacetType type = GGX);
 
 /**
  * smith occlusion function
@@ -157,43 +107,7 @@ template<EPort p = EPort::D>
 
 template<EPort p = EPort::D>
 [[nodiscard]] oc_float3<p> sample_wh(const oc_float3<p> &wo, const oc_float2<p> &u,
-                                     oc_float<p> alpha_x, oc_float<p> alpha_y, MicrofacetType type = GGX) {
-    switch (type) {
-        case Disney:
-        case GGX: {
-            oc_float<p> cos_theta = 0, phi = _2Pi * u[1];
-            phi = atan(alpha_y / alpha_x * tan(_2Pi * u[1] + PiOver2));
-            phi = select(u[1] > .5f, phi + Pi, phi);
-            oc_float<p> sin_phi = sin(phi), cos_phi = cos(phi);
-            oc_float<p> alpha2 = 1.f / (sqr(cos_phi / alpha_x) + sqr(sin_phi / alpha_y));
-            oc_float<p> tan_theta_2 = alpha2 * u[0] / (1 - u[0]);
-            cos_theta = 1 / sqrt(1 + tan_theta_2);
-            oc_float<p> sin_theta = safe_sqrt(1 - sqr(cos_theta));
-            oc_float3<p> wh = spherical_direction<p>(sin_theta, cos_theta, phi);
-            wh = select(same_hemisphere(wo, wh), wh, -wh);
-            //            CHECK_UNIT_VEC(wh)
-            return wh;
-        }
-        case Beckmann: {
-            oc_float<p> tan_theta_2, phi;
-            oc_float<p> log_sample = log(1 - u[0]);
-            phi = atan(alpha_y / alpha_x *
-                       tan(_2Pi * u[1] + PiOver2));
-            phi = select(u[1] > .5f, phi + Pi, phi);
-            oc_float<p> sin_phi = sin(phi), cos_phi = cos(phi);
-            tan_theta_2 = -log_sample / (sqr(cos_phi / alpha_x) + sqr(sin_phi / alpha_y));
-            oc_float<p> cos_theta = 1 / sqrt(1 + tan_theta_2);
-            oc_float<p> sin_theta = safe_sqrt(1 - sqr(cos_theta));
-            oc_float3<p> wh = spherical_direction<p>(sin_theta, cos_theta, phi);
-            wh = select(same_hemisphere(wo, wh), wh, -wh);
-            //            CHECK_UNIT_VEC(wh)
-            return wh;
-        }
-        default:
-            break;
-    }
-    return {};
-}
+                                     oc_float<p> alpha_x, oc_float<p> alpha_y, MicrofacetType type = GGX);
 
 template<EPort p = EPort::D>
 [[nodiscard]] oc_float<p> PDF_wh(const oc_float3<p> &wo, const oc_float3<p> &wh,
@@ -243,7 +157,7 @@ template<EPort p = EPort::D>
 }
 
 [[nodiscard]] inline DynamicArray<float> PDF_wi_transmission(const Float &PDF_wh, const Float3 &wo, const Float3 &wh,
-                                                      const Float3 &wi, const DynamicArray<float> &etas) {
+                                                             const Float3 &wi, const DynamicArray<float> &etas) {
     DynamicArray<float> denom = sqr(dot(wi, wh) * etas + dot(wo, wh));
     DynamicArray<float> dwh_dwi = abs_dot(wi, wh) / denom;
     DynamicArray<float> ret = PDF_wh * dwh_dwi;
@@ -275,20 +189,11 @@ template<EPort p = EPort::D>
 template<EPort p = EPort::D>
 [[nodiscard]] oc_float<p> BTDF_div_ft(const oc_float3<p> &wo, const oc_float3<p> &wh, const oc_float3<p> &wi,
                                       const oc_float<p> &eta, const oc_float<p> &alpha_x,
-                                      const oc_float<p> &alpha_y, MicrofacetType type = GGX) {
-    oc_float<p> cos_theta_i = cos_theta(wi);
-    oc_float<p> cos_theta_o = cos_theta(wo);
-    oc_float<p> numerator = D_<p>(wh, alpha_x, alpha_y, type) * G_<p>(wo, wi, alpha_x, alpha_y, type) *
-                            abs(dot(wi, wh) * dot(wo, wh));
-    oc_float<p> denom = sqr(dot(wi, wh) * eta + dot(wo, wh)) * abs(cos_theta_i * cos_theta_o);
-    oc_float<p> ft = numerator / denom;
-    oc_float<p> factor = rcp(sqr(eta));
-    return ft * factor;
-}
+                                      const oc_float<p> &alpha_y, MicrofacetType type = GGX);
 
 [[nodiscard]] Float BTDF_div_ft(const Float3 &wo, const Float3 &wh, const Float3 &wi,
-                                      const Float &eta, const Float &alpha_x,
-                                      const Float &alpha_y, MicrofacetType type = GGX);
+                                const Float &eta, const Float &alpha_x,
+                                const Float &alpha_y, MicrofacetType type = GGX);
 
 /**
  *
@@ -345,12 +250,12 @@ public:
     }
 
     [[nodiscard]] virtual DynamicArray<float> PDF_wi_transmission(const Float &pdf_wh, const Float3 &wo, const Float3 &wh,
-                                                           const Float3 &wi, const DynamicArray<float> &etas) const noexcept {
+                                                                  const Float3 &wi, const DynamicArray<float> &etas) const noexcept {
         return microfacet::PDF_wi_transmission(pdf_wh, wo, wh, wi, etas);
     }
 
     [[nodiscard]] virtual DynamicArray<float> PDF_wi_transmission(const Float3 &wo, const Float3 &wh,
-                                                           const Float3 &wi, const DynamicArray<float> &etas) const noexcept {
+                                                                  const Float3 &wi, const DynamicArray<float> &etas) const noexcept {
         return PDF_wi_transmission(PDF_wh(wo, wh), wo, wh, wi, etas);
     }
 
