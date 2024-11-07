57c6b172639fea94f664af0149d68c835ccc09d1
Zero <249002031@qq.com>
2024-11-08 00:06:31 +0800

    ，

--- src/pylibs/test.py
+++ src/pylibs/test.py
@@ -86,22 +86,74 @@ buffer = None
 
 import traceback
 
-d = ocapi.device()
+print("------------------------")
+
+import ast
+import inspect
+
+class MyVisitor(ast.NodeVisitor):
+    def visit_FunctionDef(self, node):
+        print(f"Function name: {node.name}")
+        self.generic_visit(node)  # 继续遍历子节点
+
+    def visit_Return(self, node):
+        print("Return statement found")
+        self.generic_visit(node)  # 继续遍历子节点
+
+    def visit_BinOp(self, node):
+        print("Binary operation found")
+        self.generic_visit(node)  # 继续遍历子节点
+
+    def visit_For(self, node):
+        print(node.target.id)
+        self.generic_visit(node)
+
+    def visit_Name(self, node):
+        print(f"Variable name: {node.id}")
+        self.generic_visit(node)  # 继续遍历子节点
+
+    def visit_Constant(self, node):
+        print(f"Constant value: {node.value}")
+        self.generic_visit(node)  # 继续遍历子节点
 
 
-def on_mouse(*arg):
-    print(*arg)
+
+# 定义一个示例函数
+def example_function(x: int):
+    # x = v.xxyz
+    return x * 2
+
+example_function(5)
+# # 获取函数的源代码
+# source_code = inspect.getsource(example_function)
+
+# # 解析源代码为 AST
+# parsed_ast = ast.parse(source_code)
+
+# visitor = MyVisitor()
+# visitor.visit(parsed_ast)
+
+# # 打印 AST
+# print(ast.dump(parsed_ast, indent=4))
+
+# d = ocapi.device()
+
+
+# def on_mouse(*arg):
+#     print(*arg)
     
 
 
-w = Window.create(500, 500)
+# w = Window.create(500, 500)
 
-w.set_mouse_callback(on_mouse)
+# # w.set_clear_color(float4(1,1,1,1))
 
-def func(t):
-    pass
+# w.set_mouse_callback(on_mouse)
+
+# def func(t):
+#     pass
 
 
-w.run(func)
+# w.run(func)
 
-ocapi.exit()
+# ocapi.exit()
