18af902b822e42794db1693303931a097f699b36
Zero <249002031@qq.com>
2024-11-09 16:49:21 +0800

    a

--- src/python/ast_parser.py
+++ src/python/ast_parser.py
@@ -1,7 +1,7 @@
 import ast
 import inspect
 
-class MyVisitor(ast.NodeVisitor):
+class OcarinaVisitor(ast.NodeVisitor):
     def visit_FunctionDef(self, node):
         print(f"Function name: {node.name}")
         self.generic_visit(node)  
@@ -9,6 +9,10 @@ class MyVisitor(ast.NodeVisitor):
     def visit_Return(self, node):
         print("Return statement found")
         self.generic_visit(node)  
+
+    def visit_Expression(self, node):
+        print("visit_Expression found")
+        self.generic_visit(node)
 
     def visit_BinOp(self, node):
         print("Binary operation found")
@@ -26,9 +30,45 @@ class MyVisitor(ast.NodeVisitor):
         print(f"Constant value: {node.value}")
         self.generic_visit(node)  
 
+class Test:
+    a : int
 
+def func(x : int):
+    t = Test()
+    t.a = x + 5
+    return x + x * 2;
 
 def example_function(x: int):
+    x += 5;
+    a = x + x
+    if x > 10:
+        a = 9
+    elif x < 6:
+        a = x
+    else:
+        a = x + a
+        
+    for i in range(0, 10, 2):
+        x += i
+    
+    arr = [1,2,4]
+    
+    arr2 = [i for i in range(x)]
+    
+    x += func(arr[1])
+    
+    x = x << 9
+
+    a = 10 if x > 9 else 15
+
+    t = Test()
+    t.a = x + 5
+    
+    while(x > 10):
+        x -= 1
+    
+    b = -x > 0
+    
     return x * 2
 
 
@@ -36,7 +76,7 @@ source_code = inspect.getsource(example_function)
 
 parsed_ast = ast.parse(source_code)
 
-visitor = MyVisitor()
+visitor = OcarinaVisitor()
 visitor.visit(parsed_ast)
 
 print(ast.dump(parsed_ast, indent=4))
