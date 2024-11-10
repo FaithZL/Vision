764165f5555786b9921e47209d0b679b1c386bc3
Zero <249002031@qq.com>
2024-11-10 11:49:17 +0800

    update

--- src/python/test.py
+++ src/python/test.py
@@ -1,3 +1,4 @@
+
 import cpplibs
 import os
 from cpplibs import ocapi
@@ -6,126 +7,138 @@ from cpplibs import vsapi
 from cpplibs.vsapi import *
 import numpy as np
 
-f2 = cpplibs.ocapi.float2(1,2)
-print(f2)
-f2[0] = 3.88999
-print(f2.x)
-print(f2.xx)
-f2.xy = cpplibs.ocapi.float2(5,6)
-print(f2.xx)
-print(f2.xy)
-print(f2.xyx * f2.xyx)
+def main():
+    
 
-f3 = f2.xyx
+    f2 = cpplibs.ocapi.float2(1,2)
+    print(f2)
+    f2[0] = 3.88999
+    print(f2.x)
+    print(f2.xx)
+    f2.xy = cpplibs.ocapi.float2(5,6)
+    print(f2.xx)
+    print(f2.xy)
+    print(f2.xyx * f2.xyx)
 
-print(cpplibs.ocapi.lerp(f3 * 0 , f3 / 2 , f3))
-print(cpplibs.ocapi.make_bool3(f3))
-print(as_float3(ocapi.as_uint3(f3)))
+    f3 = f2.xyx
 
-m = float2x2()
-m[0] = make_float2(1,6)
+    print(cpplibs.ocapi.lerp(f3 * 0 , f3 / 2 , f3))
+    print(cpplibs.ocapi.make_bool3(f3))
+    print(as_float3(ocapi.as_uint3(f3)))
 
-print(m)
-print(float3([1,22,0]))
-print(float2x2([float2(1,2),float2(3,4)]))
-print(inverse(inverse(float2x2([float2(1,2),float2(3,4)]))))
-print(make_float2x2([(1,2),(3,5)]) * 2)
-print(make_float2x2([(1,2),(3,5)]) * make_float2x2([(1,2),(3,5)]))
-print(make_float2x2([(1,2),(3,5)]) * make_float2x2([(1,2),(3,5)])[1])
-print(make_float2x2(make_float2x2([(1,2),(3,5)])).clone())
-cpplibs.init_context("cuda")
+    m = float2x2()
+    m[0] = make_float2(1,6)
 
-
-
-
-
-print("wocao")
-# t = Type.from_desc(float2().desc())
-# print(t.description())
-# print(t.name())
-# print(as_float(as_uint(2.0)))
-# print(as_float(as_uint(2.0)))
-# print(as_float(as_uint(2.0)))
-buffer = Bufferfloat2.create(2)
-# print(buffer.size())
-buffer.upload([float2(3, 4), float2(10, 11)])
-
-af = Arrayfloat2()
-af.resize(5)
-
-print(af)
-# af.push_back(float2(5.5, 10000))
-# af.push_back(float2(5.5, 999))
-# print(af[0])
-# print(af)
-# af.clear()
-# print(af)
-# # print(Arrayfloat())
-
-arr = np.array([[1.0, 5.5], [5,9]], dtype=np.float32)
-# arr = [1.0, 5.5]
-
-buffer.download(arr)
-print(arr)
-print(buffer.download())
+    print(m)
+    print(float3([1,22,0]))
+    print(float2x2([float2(1,2),float2(3,4)]))
+    print(inverse(inverse(float2x2([float2(1,2),float2(3,4)]))))
+    print(make_float2x2([(1,2),(3,5)]) * 2)
+    print(make_float2x2([(1,2),(3,5)]) * make_float2x2([(1,2),(3,5)]))
+    print(make_float2x2([(1,2),(3,5)]) * make_float2x2([(1,2),(3,5)])[1])
+    print(make_float2x2(make_float2x2([(1,2),(3,5)])).clone())
+    cpplibs.init_context("cuda")
 
 
-# a = arr[1]
-
-# # buffer.download()
-
-# # print(a2)
-
-# # lst = []
-# # vsapi.test(lst)
-
-# print(lst)
-# th = TriangleHit()
-# th.bary = float3(1,2,3).xz
-# print(Ray(float3(1,2,3), float3(1,2,5)).origin)
-buffer = None
-
-import traceback
-
-# import ast_parser
-
-# d = ocapi.device()
 
 
-def on_mouse(*arg):
-    print(*arg)
+
+    print("wocao")
+    # t = Type.from_desc(float2().desc())
+    # print(t.description())
+    # print(t.name())
+    # print(as_float(as_uint(2.0)))
+    # print(as_float(as_uint(2.0)))
+    # print(as_float(as_uint(2.0)))
+    buffer = Bufferfloat2.create(2)
+    # print(buffer.size())
+    buffer.upload([float2(3, 4), float2(10, 11)])
+
+    af = Arrayfloat2()
+    af.resize(5)
+
+    print(af)
+    # af.push_back(float2(5.5, 10000))
+    # af.push_back(float2(5.5, 999))
+    # print(af[0])
+    # print(af)
+    # af.clear()
+    # print(af)
+    # # print(Arrayfloat())
+    
+    f3 = TriangleHit()
+    
+    print(f3.to_bytes())
+    print(TriangleHit.from_bytes(f3.to_bytes()))
+    return
+
+    arr = np.array([[1.0, 5.5], [5,9]], dtype=np.float32)
+    # arr = [1.0, 5.5]
+
+    buffer.download(arr)
+    print(arr)
+    print(buffer.download())
+
+
+    # a = arr[1]
+
+    # # buffer.download()
+
+    # # print(a2)
+
+    # # lst = []
+    # # vsapi.test(lst)
+
+    # print(lst)
+    # th = TriangleHit()
+    # th.bary = float3(1,2,3).xz
+    # print(Ray(float3(1,2,3), float3(1,2,5)).origin)
+    buffer = None
+
+    import traceback
+
+    # import ast_parser
+
+    # d = ocapi.device()
+
+
+    def on_mouse(*arg):
+        print(*arg)
+        
+        
+        
+        
+    # fn = os.getcwd() + "/res/render_scene/cbox/dispersion-hero-2000.exr"
+    fn = os.getcwd() + "/res/render_scene/cbox/dispersion-hero.png"
+
+    image = Image.load(fn, ColorSpace.LINEAR)
+
+    iaar = image.as_uchar_array_t()
+
+    tex = Texture.create(image.resolution, image.pixel_storage)
+
+    tex.upload(iaar)
+
+    tarr = np.ones(1024 **2 * 4, dtype=np.uint8)
+
+    tex.download(tarr)
+
+    print(tarr)
+    res = image.resolution
+
+    w = Window.create(res)
+    w.set_clear_color(float4(1,0,0,1))
+
+    w.set_mouse_callback(on_mouse)
+
+    def func(t):
+        w.set_background(tarr)
+        pass
+
+    w.run(func)
+
     
     
-    
-    
-# fn = os.getcwd() + "/res/render_scene/cbox/dispersion-hero-2000.exr"
-fn = os.getcwd() + "/res/render_scene/cbox/dispersion-hero.png"
-
-image = Image.load(fn, ColorSpace.LINEAR)
-
-iaar = image.as_uchar_array_t()
-
-tex = Texture.create(image.resolution, image.pixel_storage)
-
-tex.upload(iaar)
-
-tarr = np.ones(1024 **2 * 4, dtype=np.uint8)
-
-tex.download(tarr)
-
-print(tarr)
-res = image.resolution
-
-w = Window.create(res)
-
-w.set_clear_color(float4(1,0,0,1))
-
-w.set_mouse_callback(on_mouse)
-
-def func(t):
-    w.set_background(tarr)
-    pass
-
-w.run(func)
+main()
 
 ocapi.exit()
