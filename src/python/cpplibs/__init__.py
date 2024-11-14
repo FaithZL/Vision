2feae48007449e3c79760f8f9235df3d60c4066b
zhuling <249002031@qq.com>
2024-11-14 15:10:07 +0800

    update

--- src/python/cpplibs/__init__.py
+++ src/python/cpplibs/__init__.py
@@ -6,22 +6,6 @@ from . import _type
 
 package_path = os.path.dirname(__file__)
 
-def to_floats(arg):
-    if type(arg) == float:
-        return arg
-    elif type(arg) == int:
-        return ocapi.as_float(arg)
-    else:
-        return arg.to_floats()
-
-def from_floats(type_, arr):
-    if type_ == int:
-        return ocapi.as_uint(arr[0])
-    elif type_ == float:
-        return arr[0]
-    else:
-        return type_.from_floats(arr)
-
 def sizeof(type_):
     if type_ == float:
         return 4
@@ -45,76 +29,8 @@ def desc(type_):
         return "int"
     else:
         return type_.desc()
-
-class StructDynamicArray(ocapi.StructDynamicArrayImpl):    
-    def __init__(self, type_):
-        super().__init__()
-        self.__type = type_
     
-    def push_back(self, elm):
-        arr = to_floats(elm)
-        super().push_back_(arr)
-        
-    def at(self, index):
-        return self[index]
-    
-    def pop_back(self):
-        size = int(sizeof(self.__type))
-        super().pop_back_(size)
-        
-    def size(self):
-        return int(super().size_in_byte() / sizeof(self.__type))
-    
-    def resize(self, num):
-        super().resize_(num * sizeof(self.__type))
-        
-    def __getitem__(self, index):
-        assert (index < self.size())
-        size = sizeof(self.__type)
-        ofs = index * size
-        return from_floats(self.__type, super().load(ofs, size))
-    
-    def __setitem__(self, index, elm):
-        assert (index < self.size())
-        size = sizeof(self.__type)
-        ofs = index * size
-        super().store(ofs, to_floats(elm))
-        
-    def __repr__(self):
-        ret = "["
-        for i in range(self.size()):
-            ret += repr(self[i]) + ","
-            if (i > 100):
-                ret += "......" + repr(self[self.size() - 1])
-                break
-        return ret + "]"
-        
-class DynamicArray:
-    def __init__(self, type_):
-        if type_ == int:
-            self.__impl = ocapi.DynamicArrayint()
-        elif type_ == float:
-            self.__impl = ocapi.DynamicArrayfloat()
-        else:
-            self.__impl = StructDynamicArray(type_)
-
-    def __getitem__(self, index):
-        return self.__impl[index]
-    
-    def __setitem__(self, index, elm):
-        self.__impl[index] = elm
-
-    def __getattr__(self, name):
-        return getattr(self.__impl, name)
-    
-    def impl(self):
-        return self.__impl
-    
-    def __repr__(self):
-        return repr(self.__impl)
-    
-
-class StructBuffer(ocapi.ByteBuffer):
+class Buffer(ocapi.ByteBuffer):
     def __init__(self, type, size):
         self.__type = type
         super().__init__(size * sizeof(self.__type))
@@ -124,57 +40,6 @@ class StructBuffer(ocapi.ByteBuffer):
     
     def type(self):
         return self.__type
-
-class PyBuffer:
-    def __init__(self, type_, size):
-        self.__type = type_
-        if type_ == int:
-            self.__impl = ocapi.Bufferint(size)
-        elif type_ == float:
-            self.__impl = ocapi.Bufferfloat(size)
-        else:
-            self.__impl = StructBuffer(type_, size)
-            
-    def type(self):
-        return self.__type
-    
-    def download_immediately(self, array=None):
-        if array is None:
-            ret = DynamicArray(self.__type)
-            ret.resize(self.__impl.size())
-            self.download_immediately(ret.as_float_array_t())
-            return ret
-        else:
-            self.__impl.download_immediately(array)
-    
-    def handle(self):
-        return self.__impl.handle()
-    
-    def size(self):
-        return self.__impl.size()
-    
-    def upload(self, data):
-        return self.__impl.upload(data)
-    
-    def download(self, data):
-        return self.__impl.download(data)
-    
-    def __getattr__(self, name):
-        return getattr(self.__impl, name)
-    
-    def upload_immediately(self, array):
-        self.__impl.upload_immediately(array)
-
-class PyMesh(ocapi.RHIMesh):
-    def __init__(self, vert_buffer : PyBuffer, tri_buffer: PyBuffer):
-        params = ocapi.MeshParams()
-        params.vert_handle = vert_buffer.handle()
-        params.vert_stride = sizeof(vert_buffer.type())
-        params.vert_num = vert_buffer.size()
-        params.tri_handle = tri_buffer.handle()
-        params.vert_stride = sizeof(tri_buffer.type())
-        params.tri_num = tri_buffer.size()
-        super().__init__(params)
 
 def init_context(backend):
     ocapi.init_context(backend, package_path)
--- src/python/test.py
+++ src/python/test.py
@@ -28,142 +28,13 @@ class Array:
         return f'array<{cpplibs.desc(self._type)},{len(self)}>'
     
 def main():
-    hit = TriangleHit()
-    hit.inst_id = 1
-    print(hit.to_bytes())
-    print(TriangleHit.from_bytes(hit.to_bytes()))
-    f3 = Array(float2, 2)
-    f3[0] = make_float2(2,3)
-    # print(f3.desc())
-    # return
     cpplibs.init_context("cuda")
-    ocapi.device().init_rtx()
-    
-    f3b = cpplibs.PyBuffer(float3, 4)
-    i3b = cpplibs.PyBuffer(uint3, 2)
-    
-    mesh = cpplibs.PyMesh(f3b, i3b)
-    accel = Accel()
-    accel.add_instance(mesh, float4x4())
-    
-    # print(accel.triangle_num())
-    # ocapi.stream().add(mesh.build_bvh()).add(accel.build_bvh()).sync().commit()
-    
-    v = cpplibs.DynamicArray(Ray)
-    v.push_back(Ray(make_float3(12,3,4),make_float3(12,3,4)))
-    v.push_back(Ray(make_float3(12,3,18),float3(0)))
-    print(v[0])
-    print(v[1])
-    v[0] = v[1]
-    print(v.size())
-    print(v)
-    # return
-    bb = cpplibs.PyBuffer(Ray, 2)
-    ocapi.stream().add(bb.upload(v.impl().as_float_array_t()))
-    # v.pop_back()
-    print(v)
-    db = cpplibs.DynamicArray(Ray)
-    db.resize(2)
-    ocapi.stream().add(bb.download(db.impl().as_float_array_t())).sync().commit()
-
-    print(db)
-    # print(v[0])
-    # print(v[1])
-    f2 = cpplibs.ocapi.float2(1,2)
-    print(f2)
-    f2[0] = 3.88999
-    print(f2.x)
-    print(f2.xx)
-    f2.xy = cpplibs.ocapi.float2(5,6)
-    print(f2.xx)
-    print(f2.xy)
-    print(f2.xyx * f2.xyx)
-
-    f3 = f2.xyx
-
-    print(cpplibs.ocapi.lerp(f3 * 0 , f3 / 2 , f3))
-    print(cpplibs.ocapi.make_bool3(f3))
-    print(as_float3(ocapi.as_uint3(f3)))
-
-    m = float2x2()
-    m[0] = make_float2(1,6)
-
-    print(m)
-    print(float3([1,22,0]))
-    print(float2x2([float2(1,2),float2(3,4)]))
-    print(inverse(inverse(float2x2([float2(1,2),float2(3,4)]))))
-    print(make_float2x2([(1,2),(3,5)]) * 2)
-    print(make_float2x2([(1,2),(3,5)]) * make_float2x2([(1,2),(3,5)]))
-    print(make_float2x2([(1,2),(3,5)]) * make_float2x2([(1,2),(3,5)])[1])
-    print(make_float2x2(make_float2x2([(1,2),(3,5)])).clone())
-    
-
-
-
-
-
-    print("wocao")
-    # t = Type.from_desc(float2().desc())
-    # print(t.description())
-    # print(t.name())
-    # print(as_float(as_uint(2.0)))
-    # print(as_float(as_uint(2.0)))
-    # print(as_float(as_uint(2.0)))
-    buffer = cpplibs.PyBuffer(float, 4)
-    # print(buffer.size())
-    tmp = np.array([3,4, 4,5], dtype=np.float32)
-    # buffer.upload_immediately(tmp)
-    ocapi.stream().add(buffer.upload(tmp))
-
-    # af.push_back(float2(5.5, 10000))
-    # af.push_back(float2(5.5, 999))
-    # print(af[0])
-    # print(af)
-    # af.clear()
-    # print(af)
-    # # print(Arrayfloat())
-    
-    f3 = Ray(float3(1), float3(6))
-    
-    print(f3.to_floats())
-    print(Ray.from_floats(f3.to_floats()))
-
-    arr = np.array([1.0, 5.5, 5,9], dtype=np.float32)
-    # arr = [1.0, 5.5]
-
-    # buffer.download_immediately(arr)
-    ocapi.stream().add(buffer.download(arr)).sync().commit()
-    print(arr)
-    print(buffer.download_immediately())
-    # return
-
-
-    # a = arr[1]
-
-    # # buffer.download_immediately()
-
-    # # print(a2)
-
-    # # lst = []
-    # # vsapi.test(lst)
-
-    # print(lst)
-    # th = TriangleHit()
-    # th.bary = float3(1,2,3).xz
-    # print(Ray(float3(1,2,3), float3(1,2,5)).origin)
-    buffer = None
-
-    import traceback
-
-    # import ast_parser
-
-    # d = ocapi.device()
+    b = to_bytes(2300000000)
+    print(bytes2uint(b))
 
 
     def on_mouse(*arg):
         print(*arg)
-        
-        
         
         
     # fn = os.getcwd() + "/res/render_scene/cbox/dispersion-hero-2000.exr"
@@ -171,17 +42,9 @@ def main():
 
     image = Image.load(fn, ColorSpace.LINEAR)
 
-    iaar = image.as_uchar_array_t()
+    iaar = image.as_uchar_array()
+    # iaar = image.as_float_array()
 
-    tex = Texture.create(image.resolution, image.pixel_storage)
-
-    tex.upload_immediately(iaar)
-
-    tarr = np.ones(1024 **2 * 4, dtype=np.uint8)
-
-    tex.download_immediately(tarr)
-
-    print(tarr)
     res = image.resolution
 
     w = Window.create(res)
@@ -190,9 +53,7 @@ def main():
     w.set_mouse_callback(on_mouse)
 
     def func(t):
-        w.set_background(tarr)
-        pass
-
+        w.set_background(iaar)
     w.run(func)
 
     
