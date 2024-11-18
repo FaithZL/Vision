d6c720a9dd998268cd795fbd9dd3197f85691ed0
zhuling <249002031@qq.com>
2024-11-18 12:21:27 +0800

    Update __init__.py

--- exporters/b2v/__init__.py
+++ exporters/b2v/__init__.py
@@ -12,7 +12,7 @@ import bpy
 
 
 class VisionRender(bpy.types.RenderEngine):
-    bl_idname = "MY_CUSTOM_RENDER"
+    bl_idname = "VISION_RENDER_ENGINE"
     bl_label = "VisionRender"
     bl_use_preview = True  # 支持材质预览
 
@@ -22,14 +22,15 @@ class VisionRender(bpy.types.RenderEngine):
         # 模拟渲染结果（显示纯黑色图像）
         result = self.begin_result(0, 0, context.region.width, context.region.height)
         layer = result.layers[0].passes["Combined"]
-        layer.rect = [[(0, 0, 0, 1)] * context.region.width] * context.region.height
+        layer.rect = [[(0, 1, 0, 1)] * context.region.width] * context.region.height
         self.end_result(result)
 
 
 def register():
-    
+    print("wocao register")
     bpy.utils.register_class(VisionRender)
     # 将自定义渲染器注册到渲染器选择菜单
+    print(dir(bpy.types.Scene))
     # bpy.types.Scene.render.engine = "MY_CUSTOM_RENDER"
 
 
