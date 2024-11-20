b9c62e78cbf1d01dac56708a480ebd8a72f6237d
zhuling <249002031@qq.com>
2024-11-20 11:55:29 +0800

    Update ui.py

--- python/b2v/ui.py
+++ python/b2v/ui.py
@@ -2,28 +2,35 @@ import bpy
 # from bpy.types import Operator
 # from bpy.props import EnumProperty
 
-# class MyDropdownOperator(Operator):
-#     bl_idname = "object.my_dropdown_operator"
-#     bl_label = "My Dropdown Operator"
-#     bl_description = "An operator with a dropdown menu"
-    
-#     # 定义下拉框的属性
-#     my_dropdown = EnumProperty(
-#         name="My Dropdown",
-#         description="Choose an option",
-#         items=[
-#             ('OPTION_ONE', "Option One", "Description of Option One"),
-#             ('OPTION_TWO', "Option Two", "Description of Option Two"),
-#             ('OPTION_THREE', "Option Three", "Description of Option Three")
-#         ],
-#         default='OPTION_ONE'
-#     )
-    
-#     # # 定义操作符的执行逻辑
-#     # def execute(self, context):
-#     #     print(f"Selected option: {self.my_dropdown}")
-#     #     return {'FINISHED'}
 
+import bpy
+from bpy.props import EnumProperty
+
+# 定义操作符的类
+class MyEnumOperator(bpy.types.Operator):
+    """示例操作符，展示下拉框"""
+    bl_idname = "object.my_enum_operator"
+    bl_label = "My Enum Operator"
+    bl_description = "这是一个包含下拉框的示例操作符"
+    bl_options = {'REGISTER', 'UNDO'}
+
+    # 定义一个枚举属性（下拉框）
+    my_enum: EnumProperty(
+        name="My Enum",
+        description="选择一个选项",
+        items=[
+            ('OPTION_ONE', "Option One", "描述一"),
+            ('OPTION_TWO', "Option Two", "描述二"),
+            ('OPTION_THREE', "Option Three", "描述三")
+        ],
+        default='OPTION_ONE'
+    )
+
+    def execute(self, context):
+        print(f"选中的选项是: {self.my_enum}")
+        return {'FINISHED'}
+    
+    
 class VisionWidget:
     COMPAT_ENGINES = {'VISION_RENDER_ENGINE'}
     bl_context = "render"
@@ -41,6 +48,8 @@ class VISION_RENDER_PT_Filter(bpy.types.Panel, VisionWidget):
 
     def draw(self, context):
         layout = self.layout
+        op = layout.operator(MyEnumOperator.bl_idname, text="My Enum Operator")
+        # op.my_enum = context.object.my_enum_property
         
 class VISION_RENDER_PT_LightSampler(bpy.types.Panel, VisionWidget):
     bl_idname = "VISION_RENDER_PT_LightSampler"
