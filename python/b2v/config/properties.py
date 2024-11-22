13d104decfe184ddf521d9947139251093910985
Zero <249002031@qq.com>
2024-11-22 22:26:01 +0800

    update py

--- python/b2v/config/properties.py
+++ python/b2v/config/properties.py
@@ -16,22 +16,18 @@ import json
 with open(os.path.join(dirname(__file__), "integrators.json")) as file:
     integrator_data = json.load(file)
 with open(os.path.join(dirname(__file__), "filters.json")) as file:
-    rfilter_data = json.load(file)
+    filter_data = json.load(file)
     
 class SceneSettingItem(bpy.types.PropertyGroup):
-    filterTab = [("GaussianFilter", "GaussianFilter", "", 1),
-                   ("BoxFilter", "BoxFilter", "", 2),
-                   ("TriangleFilter", "TriangleFilter", "", 3),
-                   ("LanczosSincFilter", "LanczosSincFilter", "", 4),
-                   ("MitchellFilter", "MitchellFilter", "", 5)]
-    
-    filterType : bpy.props.EnumProperty(
-        name="type", items=filterTab, default="GaussianFilter")
-    
     @classmethod
-    def register(cls):
+    def register(cls):   
+        filter_tab = [
+            (name, name, val["description"],i) for i, (name, val) in enumerate(filter_data.items())
+        ]
+        cls.filterType = bpy.props.EnumProperty(
+            name="type", items=filter_tab)
         bpy.types.Scene.vision_setting = PointerProperty(
             name="vision_setting Render Settings",
-            description="vision_setting render settings",
+            description="vision_setting render settings",   
             type=cls,
         )
