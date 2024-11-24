1b723569a2b066e281766fa604130902ccbfb383
Zero <249002031@qq.com>
2024-11-24 18:12:04 +0800

    删除多余文件

--- python/b2v/ui.py
+++ python/b2v/ui.py
@@ -8,8 +8,66 @@ from bpy.props import (
     PointerProperty,
     StringProperty,
 )
-from . import config
-from .config import properties
+
+import os
+from os.path import basename, dirname
+import json
+
+class VisionBaseSetting(bpy.types.PropertyGroup):
+    
+    @classmethod
+    def get_dict(cls, fn):
+        with open(os.path.join(dirname(__file__), "config/" + fn)) as file:
+            ret = json.load(file)
+        return ret
+    
+    @classmethod
+    def register_impl(cls):
+        dic = cls.get_dict(cls.fn)
+        tab = [
+            (name, name, val["description"], i)
+            for i, (name, val) in enumerate(dic.items())
+        ]
+        setattr(cls, cls.attr_type, bpy.props.EnumProperty(name="type", items=tab))
+        setattr(
+            bpy.types.Scene,
+            cls.setting_name,
+            PointerProperty(
+                name=cls.setting_name,
+                description=cls.setting_name,
+                type=cls,
+            ),
+        )
+        for val in dic.values():
+            label = val["label"]
+            for param_name, param in val["parameters"].items():
+                key = label + "_" + param_name
+                property_func = getattr(bpy.props, param["type"] + "Property")
+                ppt = property_func(**param["args"])
+                setattr(cls, key, ppt)
+
+
+class VisionFilterSetting(VisionBaseSetting):
+
+    setting_name = "vision_filter_setting"
+    attr_type = "filter_type"
+    fn = "filters.json"
+
+    @classmethod
+    def register(cls):
+        cls.register_impl()
+
+
+class VisionIntegratorSetting(VisionBaseSetting):
+
+    setting_name = "vision_integrator_setting"
+    attr_type = "integrator_type"
+    fn = "integrators.json"
+
+    @classmethod
+    def register(cls):
+        cls.register_impl()
+
 
 
 class VisionWidget:
@@ -49,13 +107,13 @@ class VISION_RENDER_PT_VisionBasePanel(VisionWidget):
 class VISION_RENDER_PT_Filter(bpy.types.Panel, VISION_RENDER_PT_VisionBasePanel):
     bl_idname = "VISION_RENDER_PT_Filter"
     bl_label = "Filter"
-    property_cls = properties.VisionFilterSetting
+    property_cls = VisionFilterSetting
 
 
 class VISION_RENDER_PT_Intergrator(bpy.types.Panel, VISION_RENDER_PT_VisionBasePanel):
     bl_idname = "VISION_RENDER_PT_Intergrator"
     bl_label = "Intergrator"
-    property_cls = properties.VisionIntegratorSetting
+    property_cls = VisionIntegratorSetting
 
 
 class VISION_RENDER_PT_LightSampler(bpy.types.Panel, VisionWidget):
