becda89a1f83c6cc941dc408ec29ec696f987395
Zero <249002031@qq.com>
2024-11-24 11:58:47 +0800

    Update ui.py

--- python/b2v/ui.py
+++ python/b2v/ui.py
@@ -11,38 +11,25 @@ from bpy.props import (
 from . import config
 from .config import properties
 
+
 class VisionWidget:
     COMPAT_ENGINES = {"VISION_RENDER_ENGINE"}
     bl_context = "render"
+    bl_space_type = "PROPERTIES"
+    bl_region_type = "WINDOW"
 
     @classmethod
     def poll(cls, context):
         return context.engine in cls.COMPAT_ENGINES
 
-# class VISION_RENDER_PT_Panel(bpy.types.Panel, VisionWidget):
-    
-#     def draw(self, context):
-#         scene = context.scene
-#         layout = self.layout
-#         row = layout.row()
-#         layout.use_property_split = True
-#         layout.use_property_decorate = False
-#         vs = scene.vision_filter_setting
-#         row.prop(vs, "filter_type")
-#         cur_filter = vs.filter_type
-#         for attr in dir(vs):
-#             if attr.startswith(cur_filter):
-#                 attr_name = attr[len(cur_filter) + 1 :]
-#                 layout.row().prop(vs, attr, text=attr_name)
 
 class VISION_RENDER_PT_Filter(bpy.types.Panel, VisionWidget):
     bl_idname = "VISION_RENDER_PT_Filter"
     bl_label = "Filter"
-    bl_space_type = "PROPERTIES"
-    bl_region_type = "WINDOW"
-    
-    attr_type = "filter_type"
-    scene_key = "vision_filter_setting"
+
+    property_cls = properties.VisionFilterSetting
+    attr_type = property_cls.attr_type
+    scene_key = property_cls.setting_name
 
     def draw(self, context):
         scene = context.scene
@@ -62,8 +49,6 @@ class VISION_RENDER_PT_Filter(bpy.types.Panel, VisionWidget):
 class VISION_RENDER_PT_LightSampler(bpy.types.Panel, VisionWidget):
     bl_idname = "VISION_RENDER_PT_LightSampler"
     bl_label = "LightSampler"
-    bl_space_type = "PROPERTIES"
-    bl_region_type = "WINDOW"
 
     def draw(self, context):
         layout = self.layout
@@ -72,8 +57,6 @@ class VISION_RENDER_PT_LightSampler(bpy.types.Panel, VisionWidget):
 class VISION_RENDER_PT_Spectrum(bpy.types.Panel, VisionWidget):
     bl_idname = "VISION_RENDER_PT_Spectrum"
     bl_label = "Spectrum"
-    bl_space_type = "PROPERTIES"
-    bl_region_type = "WINDOW"
 
     def draw(self, context):
         layout = self.layout
@@ -82,8 +65,6 @@ class VISION_RENDER_PT_Spectrum(bpy.types.Panel, VisionWidget):
 class VISION_RENDER_PT_Sampler(bpy.types.Panel, VisionWidget):
     bl_idname = "VISION_RENDER_PT_Sampler"
     bl_label = "Sampler"
-    bl_space_type = "PROPERTIES"
-    bl_region_type = "WINDOW"
 
     def draw(self, context):
         layout = self.layout
@@ -92,8 +73,6 @@ class VISION_RENDER_PT_Sampler(bpy.types.Panel, VisionWidget):
 class VISION_RENDER_PT_Intergrator(bpy.types.Panel, VisionWidget):
     bl_idname = "VISION_RENDER_PT_Intergrator"
     bl_label = "Intergrator"
-    bl_space_type = "PROPERTIES"
-    bl_region_type = "WINDOW"
 
     def draw(self, context):
         scene = context.scene
@@ -112,8 +91,6 @@ class VISION_RENDER_PT_Intergrator(bpy.types.Panel, VisionWidget):
 class VISION_RENDER_PT_Operator(bpy.types.Panel, VisionWidget):
     bl_idname = "VISION_RENDER_PT_Operator"
     bl_label = "Operator"
-    bl_space_type = "PROPERTIES"
-    bl_region_type = "WINDOW"
 
     def draw(self, context):
         layout = self.layout
