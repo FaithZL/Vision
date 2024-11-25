5b161e9a9ebc55690534ca31f0dab88c9a83ea7e
zhuling <249002031@qq.com>
2024-11-25 17:56:57 +0800

    Create exporter.py

---
+++ python/b2v/exporter.py
@@ -0,0 +1,71 @@
+import bpy
+from bpy.props import (
+        StringProperty,
+        BoolProperty,
+    )
+from bpy_extras.io_utils import (
+        ExportHelper,
+        orientation_helper,
+        axis_conversion
+    )
+
+
+
+@orientation_helper(axis_forward='-Z', axis_up='Y')
+class ExportVision(bpy.types.Operator, ExportHelper):
+    bl_idname = "export_scene.vision"
+    bl_label = "Vision Export"
+
+    filename_ext = ".json"
+    filter_glob: StringProperty(default="*.json", options={'HIDDEN'})
+
+    use_selection: BoolProperty(
+	        name = "Selection Only",
+	        description="Export selected objects only",
+	        default = False,
+	    )
+
+    split_files: BoolProperty(
+            name = "Split File",
+            description = "Split scene XML file in smaller fragments",
+            default = False
+    )
+
+    export_ids: BoolProperty(
+            name = "Export IDs",
+            description = "Add an 'id' field for each object (shape, emitter, camera...)",
+            default = False
+    )
+
+    ignore_background: BoolProperty(
+            name = "Ignore Default Background",
+            description = "Ignore blender's default constant gray background when exporting to Mitsuba.",
+            default = True
+    )
+
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        pass
+
+    def execute(self, context):
+        axis_mat = axis_conversion(
+	            to_forward=self.axis_forward,
+	            to_up=self.axis_up,
+	        ).to_4x4()
+        
+        print(axis_mat)
+
+        return {'FINISHED'}
+
+
+def menu_export_func(self, context):
+    self.layout.operator(ExportVision.bl_idname, text="vision (.json)")
+
+
+def register():
+    bpy.types.TOPBAR_MT_file_export.append(menu_export_func)
+
+def unregister():
+    bpy.types.TOPBAR_MT_file_export.remove(menu_export_func)
