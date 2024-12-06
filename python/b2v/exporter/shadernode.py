3d87cc8bd09a7c70e573d2094538cc6e2890302c
Zero <249002031@qq.com>
2024-12-07 00:15:37 +0800

    Update shadernode.py

--- python/b2v/exporter/shadernode.py
+++ python/b2v/exporter/shadernode.py
@@ -1,6 +1,7 @@
 import bpy
 import os
 import json
+import shutil
 from bpy.props import (
     BoolProperty,
     CollectionProperty,
@@ -12,14 +13,28 @@ from bpy.props import (
 )
 
 
-def parse_image_node(exporter, socket, dim):
-    ret = {}
-    print("-----------------------", socket.type)
+def parse_image_node(exporter, from_node, dim):
+    exporter.try_make_tex_dir()
+    src_path = bpy.path.abspath(from_node.image.filepath)
+    dst_path = exporter.texture_path(from_node.image.name)
+    shutil.copyfile(src_path, dst_path)
+    r_path = exporter.tex_dir + "/" + from_node.image.name
+    ret = {
+        "channels": "xyz",
+        "node": {"type": "image", "param": {"fn": r_path, "color_space": "srgb"}},
+    }
+
     return ret
 
 
+func_dict = {"TEX_IMAGE": parse_image_node}
+
+
 def parse_node(exporter, socket, dim):
+    # print("-----------------------", socket.links)
     if socket.is_linked:
-        return parse_image_node(exporter, socket, dim)
+        from_node = socket.links[0].from_node
+        func = func_dict[from_node.type]
+        return func(exporter, from_node, dim)
     value = list(socket.default_value)[0:dim]
     return {"channels": "xyz", "node": value}
