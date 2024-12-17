24950b129741a803198db6395339e35db0191f4d
Zero <249002031@qq.com>
2024-12-17 20:01:12 +0800

    Update light.py

--- python/b2v/exporter/light.py
+++ python/b2v/exporter/light.py
@@ -11,9 +11,10 @@ from bpy.props import (
     PointerProperty,
     StringProperty,
 )
-from ..import utils
+from .. import utils
 from mathutils import Matrix, Vector
 import numpy as np
+
 
 def export_area(exporter, object):
     light = object.data
@@ -26,11 +27,10 @@ def export_area(exporter, object):
         width = light.size * object.scale.x
         height = light.size_y * object.scale.y
 
-    
-    rotation_matrix_x = Matrix.Rotation(math.radians(-90), 4, 'X')
+    rotation_matrix_x = Matrix.Rotation(math.radians(-90), 4, "X")
     mat = exporter.correct_matrix(object.matrix_world)
     mat_list = utils.matrix_to_list(mat @ rotation_matrix_x)
-    
+
     ret = {
         "type": "area",
         "param": {
@@ -38,12 +38,7 @@ def export_area(exporter, object):
             "scale": light.energy,
             "width": width,
             "height": height,
-            "o2w" : {
-                'type': 'matrix4x4',
-                'param': {
-                    'matrix4x4': mat_list
-                }
-            }
+            "o2w": {"type": "matrix4x4", "param": {"matrix4x4": mat_list}},
         },
     }
     return ret
@@ -55,11 +50,11 @@ def export_point(exporter, object):
     p = exporter.correct_matrix(pos)
     ret = {
         "type": "point",
-        "param" :{
+        "param": {
             "color": {"channels": "xyz", "node": list(light.color)},
             "scale": light.energy / (4 * np.pi),
-            "position" : list(p)
-        }
+            "position": list(p),
+        },
     }
     return ret
 
@@ -71,13 +66,20 @@ def export_spot(exporter, object):
     rotation_euler = object.rotation_euler
     direction_vector = Vector((0.0, 0.0, -1.0))
     direction_vector.rotate(rotation_euler)
+    angle = math.degrees(light.spot_size / 2.0)
+    direction = exporter.correct_matrix(direction_vector)
+    b = light.spot_blend
+    falloff = angle * b
     ret = {
         "type": "spot",
-        "param" :{
+        "param": {
             "color": {"channels": "xyz", "node": list(light.color)},
             "scale": light.energy / (4 * np.pi),
-            "position" : list(p)
-        }
+            "position": list(p),
+            "angle": angle,
+            "falloff": falloff,
+            "direction": list(direction),
+        },
     }
     return ret
 
