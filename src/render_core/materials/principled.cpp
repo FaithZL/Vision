9380017817b76bb54ed0639d46be14f37729fdaf
ling.zhu <249002031@qq.com>
2025-01-27 22:12:56 +0800

    Update principled.cpp

--- src/render_core/materials/principled.cpp
+++ src/render_core/materials/principled.cpp
@@ -294,23 +294,23 @@ public:
         Float2 alpha = make_float2(max(0.001f, sqr(roughness) / aspect),
                                    max(0.001f, sqr(roughness) * aspect));
 
-        // diffuse
-        Float diff_weight = 1 - metallic;
-        WeightedBxDFSet diffuse_lobe{diff_weight, make_shared<DiffuseBxDFSet>(color * diff_weight, swl)};
-        lobes.push_back(std::move(diffuse_lobe));
-
         // specular
         Float f0 = schlick_F0_from_eta(ior);
         SP<FresnelGeneralizedSchlick> fresnel_schlick = make_shared<FresnelGeneralizedSchlick>(f0 * specular_tint, ior, swl);
         SP<GGXMicrofacet> microfacet = make_shared<GGXMicrofacet>(alpha.x, alpha.y);
         UP<BxDF> spec_refl = make_unique<MicrofacetReflection>(SampledSpectrum::one(swl.dimension()), swl, microfacet);
         WeightedBxDFSet specular_lobe(1, make_unique<UniversalReflectBxDFSet>(fresnel_schlick, std::move(spec_refl)));
-//        lobes.push_back(std::move(specular_lobe));
+        lobes.push_back(std::move(specular_lobe));
+
+        // diffuse
+        Float diff_weight = 1 - metallic;
+        WeightedBxDFSet diffuse_lobe{diff_weight, make_shared<DiffuseBxDFSet>(color * diff_weight, swl)};
+        lobes.push_back(std::move(diffuse_lobe));
 
         // metallic
         SP<FresnelF82Tint> fresnel_f82 = make_shared<FresnelF82Tint>(color, swl);
         fresnel_f82->init_from_F82(specular_tint);
-        UP<BxDF> metal_refl = make_unique<MicrofacetReflection>(SampledSpectrum::one(swl.dimension()), swl, microfacet);
+        UP<BxDF> metal_refl = make_unique<MicrofacetReflection>(SampledSpectrum::one(swl.dimension()) * metallic, swl, microfacet);
         WeightedBxDFSet metal_lobe(metallic, make_unique<UniversalReflectBxDFSet>(fresnel_f82, std::move(metal_refl)));
         lobes.push_back(std::move(metal_lobe));
 
