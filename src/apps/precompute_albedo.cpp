a312ff99aee7c62acf56b5c121d63bf7a14c9336
ling.zhu <249002031@qq.com>
2025-01-31 17:47:05 +0800

    update precompute

---
+++ src/apps/precompute_albedo.cpp
@@ -0,0 +1,12 @@
+//
+// Created by ling.zhu on 2025/1/31.
+//
+
+#include "application.h"
+
+using namespace ocarina;
+using namespace vision;
+
+int main(int argc, char *argv[]) {
+    return 0;
+}
