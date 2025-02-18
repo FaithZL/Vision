cd6869d4f2823459e7b97c08fd9ad9e62532fafd
ling.zhu <249002031@qq.com>
2025-02-18 12:42:32 +0800

    Update glass.cpp

--- src/render_core/materials/glass.cpp
+++ src/render_core/materials/glass.cpp
@@ -117,13 +117,7 @@ SampledDirection DielectricBxDFSet::sample_wi(const Float3 &wo, const Uint &flag
 BSDFSample DielectricBxDFSet::sample_local(const Float3 &wo, const Uint &flag,
                                            TSampler &sampler) const noexcept {
     BSDFSample ret{*swl()};
-    auto sd = sample_wi(wo, flag, sampler);
-    ret.wi = sd.wi;
-    ret.eval = evaluate_local(wo, sd.wh, sd.wi, MaterialEvalMode::All, flag);
-    ret.eval.pdfs = ret.eval.pdf() * sd.factor();
-//    return ret;
     Float3 wh = microfacet_->sample_wh(wo, sampler->next_2d());
-//    Float3 wh = sd.wh;
     Float d = dot(wo, wh);
     auto fresnel = fresnel_->clone();
     fresnel->correct_eta(wo.z);
