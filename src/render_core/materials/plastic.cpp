f31b9e8289f8956ea8a47574f46cf685147f7780
ling.zhu <249002031@qq.com>
2025-02-22 14:53:04 +0800

    add plastic

---
+++ src/render_core/materials/plastic.cpp
@@ -0,0 +1,32 @@
+//
+// Created by ling.zhu on 2025/2/22.
+//
+
+#include "base/scattering/material.h"
+#include "base/shader_graph/shader_node.h"
+#include "base/mgr/scene.h"
+#include "math/warp.h"
+
+namespace vision {
+
+class PlasticMaterial : public Material {
+private:
+    
+protected:
+    VS_MAKE_MATERIAL_EVALUATOR(MicrofacetBxDFSet)
+public:
+    PlasticMaterial() = default;
+    explicit PlasticMaterial(const MaterialDesc &desc)
+        : Material(desc) {
+
+    }
+    VS_MAKE_PLUGIN_NAME_FUNC
+    [[nodiscard]] UP<BxDFSet> create_lobe_set(Interaction it, const SampledWavelengths &swl) const noexcept override {
+        return nullptr;
+    }
+};
+
+}// namespace vision
+
+VS_MAKE_CLASS_CREATOR_HOTFIX(vision, PlasticMaterial)
+VS_REGISTER_CURRENT_PATH(0, "vision-material-plastic.dll")
