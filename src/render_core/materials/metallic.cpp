8bdd4ebf560cba3f3d1cf51f87034f47163ffd4a
Zero <249002031@qq.com>
2025-03-15 12:05:28 +0800

    finish metallic

--- src/render_core/materials/metallic.cpp
+++ src/render_core/materials/metallic.cpp
@@ -25,7 +25,7 @@ public:
     explicit MetallicMaterial(const MaterialDesc &desc)
         : Material(desc) {
         INIT_SLOT(color, make_float3(1.f), Albedo);
-        INIT_SLOT(edge_tint, make_float3(0.f), Albedo);
+        INIT_SLOT(edge_tint, make_float3(1.f), Albedo);
         INIT_SLOT(roughness, 0.5f, Number)->set_range(0.0001f, 1.f);
         INIT_SLOT(anisotropic, 0.f, Number)->set_range(-1,1);
         init_slot_cursor(&color_, &anisotropic_);
@@ -33,7 +33,22 @@ public:
     VS_MAKE_PLUGIN_NAME_FUNC
     VS_HOTFIX_MAKE_RESTORE(Material, remapping_roughness_, alpha_threshold_)
     [[nodiscard]] UP<Lobe> create_lobe_set(Interaction it,const SampledWavelengths &swl) const noexcept override {
-        return nullptr;
+        SampledSpectrum color = color_.eval_albedo_spectrum(it, swl).sample;
+        SampledSpectrum edge_tint = edge_tint_.eval_albedo_spectrum(it, swl).sample;
+        Float roughness = ocarina::clamp(roughness_.evaluate(it, swl).as_scalar(), 0.01f, 1.f);
+        Float anisotropic = ocarina::clamp(anisotropic_.evaluate(it, swl).as_scalar(), -0.9f, 0.9f);
+
+        roughness = remapping_roughness_ ? roughness_to_alpha(roughness) : roughness;
+        Float2 alpha = calculate_alpha<D>(roughness, anisotropic);
+        Float alpha_min = min(alpha.x, alpha.y);
+        Uint flag = select(alpha_min < alpha_threshold_, SurfaceData::NearSpec, SurfaceData::Glossy);
+        auto microfacet = make_shared<GGXMicrofacet>(alpha.x, alpha.y);
+
+        SP<FresnelF82Tint> fresnel_f82 = make_shared<FresnelF82Tint>(color, swl);
+        fresnel_f82->init_from_F82(edge_tint);
+
+        UP<MicrofacetReflection> metal_refl = make_unique<MicrofacetReflection>(color,swl, microfacet);
+        return make_unique<MicrofacetLobe>(fresnel_f82, std::move(metal_refl));
     }
 };
 }// namespace vision
