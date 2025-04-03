5c63a761448acd7e61bd4bcd7cb43f5265e37f6e
Zero <249002031@qq.com>
2025-04-03 23:58:06 +0800

    update camera

---
+++ src/render_core/sensors/realistic.cpp
@@ -0,0 +1,29 @@
+//
+// Created by Zero on 2025/4/3.
+//
+
+#include "base/sensor/sensor.h"
+#include "base/mgr/pipeline.h"
+#include "GUI/widgets.h"
+
+namespace vision {
+struct Lens {
+    float curvature_radius{};
+    float thickness{};
+    float ior{};
+    float aperture_radius{};
+};
+}// namespace vision
+
+OC_STRUCT(vision, Lens, curvature_radius,
+          thickness, ior, aperture_radius){
+
+};
+
+namespace vision {
+
+class RealisticCamera : public Camera {
+private:
+};
+
+}// namespace vision
