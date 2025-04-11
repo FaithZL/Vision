747dbbeb610480562bcbfc28f958a2d2365e1d42
Zero <249002031@qq.com>
2025-04-11 11:03:44 +0800

    del usd

--- src/ext/usd/lib/python/pxr/__init__.py
+++
@@ -1,1 +0,0 @@
-__all__ = ['Tf', 'Gf', 'Trace', 'Work', 'Plug', 'Vt', 'Ar', 'Kind', 'Sdf', 'Ndr', 'Sdr', 'Pcp', 'Usd', 'UsdGeom', 'UsdVol', 'UsdMedia', 'UsdShade', 'UsdLux', 'UsdProc', 'UsdRender', 'UsdHydra', 'UsdRi', 'UsdSkel', 'UsdUI', 'UsdUtils', 'UsdPhysics', 'UsdMtlx', 'Garch', 'CameraUtil', 'PxOsd', 'GeomUtil', 'Glf', 'UsdImagingGL', 'UsdAppUtils', 'Usdviewq', 'UsdBakeMtlx']
