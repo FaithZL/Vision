97770a431323b40afa6d9e51394b555d9b84b540
Zero <249002031@qq.com>
2025-04-15 11:03:16 +0800

    Update realistic.cpp

--- src/render_core/sensors/realistic.cpp
+++ src/render_core/sensors/realistic.cpp
@@ -7,7 +7,7 @@
 #include "GUI/widgets.h"
 
 namespace vision {
-struct LensElement {
+struct LensInterface {
     float curvature_radius{};
     float thickness{};
     float eta{};
@@ -15,8 +15,24 @@ struct LensElement {
 };
 }// namespace vision
 
+static constexpr std::array lens_config = {
+    float4(35.98738, 1.21638, 1.54, 23.716),
+    float4(11.69718, 9.9957, 1, 17.996),
+    float4(13.08714, 5.12622, 1.772, 12.364),
+    float4(-22.63294, 1.76924, 1.617, 9.812),
+    float4(71.05802, 0.8184, 1, 9.152),
+    float4(0, 2.27766, 0, 8.756),
+    float4(-9.58584, 2.43254, 1.617, 8.184),
+    float4(-11.28864, 0.11506, 1, 9.152),
+    float4(-166.7765, 3.09606, 1.713, 10.648),
+    float4(-7.5911, 1.32682, 1.805, 11.44),
+    float4(-16.7662, 3.98068, 1, 12.276),
+    float4(-7.70286, 1.21638, 1.617, 13.42),
+    float4(-11.97328, 1.66, 1, 17.996),
+};
+
 // clang-format off
-OC_STRUCT(vision, LensElement, curvature_radius,
+OC_STRUCT(vision, LensInterface, curvature_radius,
           thickness, eta, aperture_radius){
     void init(const Float &c, const Float &t, const Float &e, const Float &a) noexcept {
         curvature_radius = c;
@@ -31,22 +47,21 @@ namespace vision {
 
 class RealisticCamera : public Camera {
 private:
-    ///
-    EncodedData<vector<float>> lens_elements_;
+    EncodedData<vector<float>> lens_interfaces_;
     bool simple_weighting_{};
 
 public:
     RealisticCamera() = default;
     explicit RealisticCamera(const SensorDesc &desc)
         : Camera(desc) {}
-    OC_ENCODABLE_FUNC(Camera, lens_elements_)
-    [[nodiscard]] LensElementVar lens_element(const Uint &index) const noexcept {
+    OC_ENCODABLE_FUNC(Camera, lens_interfaces_)
+    [[nodiscard]] LensInterfaceVar lens_element(const Uint &index) const noexcept {
         Uint i = index * 4;
-        Float curvature_radius = (*lens_elements_)[i];
-        Float thickness = (*lens_elements_)[i + 1];
-        Float eta = (*lens_elements_)[i + 2];
-        Float aperture_radius = (*lens_elements_)[i + 3];
-        LensElementVar ret;
+        Float curvature_radius = (*lens_interfaces_)[i];
+        Float thickness = (*lens_interfaces_)[i + 1];
+        Float eta = (*lens_interfaces_)[i + 2];
+        Float aperture_radius = (*lens_interfaces_)[i + 3];
+        LensInterfaceVar ret;
         ret->init(curvature_radius, thickness, eta, aperture_radius);
         return ret;
     }
