245b8b541cebac8986dedab41250ae31fce3b91a
Zero <249002031@qq.com>
2025-04-18 18:40:46 +0800

    update

--- src/render_core/shapes/model.cpp
+++ src/render_core/shapes/model.cpp
@@ -31,7 +31,7 @@ public:
                                desc["smooth"].as_bool(false),
                                desc["flip_uv"].as_bool(true));
         string mat_name = desc["material"].as_string();
-        auto instances = parser.parse_meshes(mat_name.empty(), desc["subdiv_level"].as_uint(0u));
+        auto instances = parser.parse_meshes(false, desc["subdiv_level"].as_uint(0u));
         add_instances(ocarina::move(instances));
     }
 };
