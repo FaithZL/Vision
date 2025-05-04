b33f83bba76aca48d99b1909779b0fa08c48df5e
Zero <249002031@qq.com>
2025-05-04 21:40:46 +0800

    Update tex_coord.cpp

--- src/render_core/shadernodes/tex_coord.cpp
+++ src/render_core/shadernodes/tex_coord.cpp
@@ -10,14 +10,14 @@ class TextureCoordinate : public InputNode {
 public:
     using InputNode::InputNode;
     VS_MAKE_PLUGIN_NAME_FUNC
-    [[nodiscard]] AttrEvalContext apply(const AttrEvalContext &ctx,
+    [[nodiscard]] AttrEvalContext apply(const AttrEvalContext &ctx, const SampledWavelengths &swl,
                                         const string &key) const noexcept override {
 
         return ctx;
     }
 };
 
-}
+}// namespace vision
 
 VS_MAKE_CLASS_CREATOR_HOTFIX(vision, TextureCoordinate)
 VS_REGISTER_CURRENT_PATH(0, "vision-shadernode-tex_coord.dll")
