5fe1b33656d2e935b7991897ff671f9309751601
Zero <249002031@qq.com>
2025-05-19 16:37:53 +0800

    Update src_node.cpp

--- src/render_core/shadernodes/src_node.cpp
+++ src/render_core/shadernodes/src_node.cpp
@@ -30,8 +30,36 @@ public:
         return float_array::from_vec(uv);
     }
 };
+//todo
+class GeometryNode : public ShaderNode {
+public:
+    using ShaderNode::ShaderNode;
+    VS_MAKE_PLUGIN_NAME_FUNC_(geometry)
+
+    [[nodiscard]] float_array evaluate(const string &key, const AttrEvalContext &ctx,
+                                       const SampledWavelengths &swl) const noexcept override {
+        Float2 uv;
+        if (key == "Position") {
+            uv = ctx.uv;
+        } else if (key == "Normal") {
+            TSensor sensor = scene().sensor();
+            uv = sensor->device_position().xy();
+        } else if (key == "Tangent") {
+            uv = ctx.pos->xy();
+        } else if (key == "True Normal") {
+            uv = make_float2(dispatch_idx().xy()) / dispatch_dim().xy();
+        } else if (key == "Incoming") {
+            uv = make_float2(dispatch_idx().xy()) / dispatch_dim().xy();
+        } else {
+            OC_WARNING_FORMAT("{} is unknown, fallback to uv", key.c_str());
+            uv = ctx.uv;
+        }
+        return float_array::from_vec(uv);
+    }
+};
 
 }// namespace vision
 
 VS_MAKE_CLASS_CREATOR_HOTFIX_FUNC(vision, TextureCoordinate, tex_coord)
-VS_REGISTER_CURRENT_PATH(0, "vision-shadernode-tex_coord.dll")
+VS_MAKE_CLASS_CREATOR_HOTFIX_FUNC(vision, GeometryNode, geometry)
+VS_REGISTER_CURRENT_PATH(0, "vision-shadernode-src_node.dll")
