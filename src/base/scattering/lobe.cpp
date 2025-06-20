c44afb9032f46f6aadd77f40ec9c224a2b3032e8
Zero <249002031@qq.com>
2025-06-21 00:08:30 +0800

    Update lobe.cpp

--- src/base/scattering/lobe.cpp
+++ src/base/scattering/lobe.cpp
@@ -33,7 +33,7 @@ SampledSpectrum Lobe::precompute_with_radio(const Float3 &ratio, TSampler &sampl
 }
 
 Float Lobe::valid_factor(const Float3 &wo, const Float3 &wi) const noexcept {
-    Bool valid = same_hemisphere(wo, wi, shading_frame().normal());
+    Bool valid = same_hemisphere(wo, wi);
     return cast<float>(valid);
 }
 
