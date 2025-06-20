dc64ce3518e568201b10cd118d5a9cc91c14f823
Zero <249002031@qq.com>
2025-06-20 18:36:14 +0800

    Update main.cpp

--- src/apps/vision-gui/main.cpp
+++ src/apps/vision-gui/main.cpp
@@ -8,5 +8,7 @@ using namespace ocarina;
 using namespace vision;
 
 int main(int argc, char *argv[]) {
+    fs::path runtime_dir = fs::path(argv[0]).parent_path();
+    fs::current_path(runtime_dir);
     return App(argc, argv).run();
 }
