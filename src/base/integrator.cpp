946463ca4541660096609ec97d53634632f32c9e
Zero <249002031@qq.com>
2025-06-27 19:02:18 +0800

    u

--- src/base/integrator.cpp
+++ src/base/integrator.cpp
@@ -147,7 +147,7 @@ Float3 IlluminationIntegrator::Li(RayState rs, Float scatter_pdf, const Uint &ma
     Float3 ret = make_float3(0.f);
 
     Float3 primary_dir = rs.direction();
-    auto mis_bsdf = [&](auto &bounces, bool inner) {
+    auto mis_bsdf = [&](auto &bounces, bool inner, auto continue_, auto break_) {
         hit = geometry.trace_closest(rs.ray);
         comment("miss");
         if (!inner) {
@@ -206,7 +206,7 @@ Float3 IlluminationIntegrator::Li(RayState rs, Float scatter_pdf, const Uint &ma
 
     Float eta_scale = 1.f;
     $for(&bounces, 0, max_depth) {
-        mis_bsdf(bounces, true);
+        mis_bsdf(bounces, true, continue_, break_);
         Env::instance().set("bounces", bounces);
         comment("estimate direct lighting");
         comment("sample light");
@@ -276,7 +276,7 @@ Float3 IlluminationIntegrator::Li(RayState rs, Float scatter_pdf, const Uint &ma
     if (only_direct && mis_mode_ == MISMode::EBoth) {
         /// Supplement only direct light BSDF sampling
         $for(&bounce, 1u) {
-            mis_bsdf(bounce, false);
+            mis_bsdf(bounce, false, continue_, break_);
         };
     }
     return ret;
--- src/render_core/denoisers/SVGF/atrous.cpp
+++ src/render_core/denoisers/SVGF/atrous.cpp
@@ -61,13 +61,13 @@ void AtrousFilter::compile() noexcept {
                 Int2 offset = cur_pixel - neighbor;
                 Bool center = all(offset == 0);
                 $if(center) {
-                    $continue;
+                    syntax::continue_();
                 };
                 Float kernel_weight = kernel_weights[abs(offset.x)] * kernel_weights[abs(offset.y)];
                 Int2 new_offset = offset * param.step_size;
                 Int2 target_pixel = cur_pixel + new_offset;
                 $if(!in_screen(target_pixel, make_int2(dispatch_dim().xy()))) {
-                    $continue;
+                    syntax::continue_();
                 };
                 Uint index = dispatch_id(target_pixel);
                 PixelGeometryVar neighbor_geom = param.gbuffer.read(index);
