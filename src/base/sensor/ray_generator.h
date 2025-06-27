692fc81bb76163d37f9f129d990bfcf9d0b16bcf
Zero <249002031@qq.com>
2025-06-27 11:26:04 +0800

    Update ray_generator.h

--- src/base/sensor/ray_generator.h
+++ src/base/sensor/ray_generator.h
@@ -13,16 +13,25 @@
 namespace vision {
 using namespace ocarina;
 
-class RayGenerator : public Node, public Observer {
+class RayGenerator : public Node, public Encodable, public Observer {
 public:
     using Desc = RayGeneratorDesc;
+
+private:
+    uint2 resolution_;
 
 public:
     RayGenerator() = default;
     explicit RayGenerator(const RayGeneratorDesc &desc)
         : Node(desc) {}
 
-    [[nodiscard]] virtual uint2 resolution() const noexcept = 0;
+    [[nodiscard]] virtual uint2 launch_dim() const noexcept { return resolution_; }
+    void set_resolution(uint2 res) noexcept { resolution_ = res; }
+    void update_resolution(uint2 res) noexcept {
+        set_resolution(res);
+    }
+    virtual void on_resize(uint2 res) noexcept {}
+    [[nodiscard]] uint pixel_num() const noexcept { return resolution_.x * resolution_.y; }
 };
 
 }// namespace vision
