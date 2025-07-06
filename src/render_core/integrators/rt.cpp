5919aa1dc7c7e72f2eaf63507dcbb2d00b43f08c
Zero <249002031@qq.com>
2025-07-06 22:14:54 +0800

    Update rt.cpp

--- src/render_core/integrators/rt.cpp
+++ src/render_core/integrators/rt.cpp
@@ -102,6 +102,12 @@ public:
         path_tracing_ = device().compile(kernel, "real_time_pt");
     }
 
+    [[nodiscard]] CommandList compute_GBuffer() const noexcept {
+        return frame_buffer().compute_GBuffer(frame_index_, frame_buffer().cur_gbuffer(frame_index_),
+                                              frame_buffer().motion_vectors(), frame_buffer().albedo(),
+                                              frame_buffer().emission());
+    }
+
     void compile() noexcept override {
         direct_->compile();
         indirect_->compile();
@@ -138,7 +144,7 @@ public:
         const Pipeline *rp = pipeline();
         Stream &stream = rp->stream();
         stream << frame_buffer().compute_hit(frame_index_);
-
+        stream << compute_GBuffer();
         stream << direct_->dispatch(frame_index_);
         //        stream << path_tracing_(frame_index_,
         //                                frame_buffer().cur_surfaces(frame_index_))
